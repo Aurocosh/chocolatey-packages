c810cf4fdad86e6abf55a129e7a45dca665510e9
Aurocosh <Aurocosh@gmail.com>
2024-01-24 23:10:02 +0700

    Additional comment cleanup

--- manual/opencomic/tools/chocolateyinstall.ps1
+++ manual/opencomic/tools/chocolateyinstall.ps1
@@ -11,7 +11,7 @@ $packageArgs = @{
   checksum64     = '52F89DD22C3FA684885F0AF32245D81AB5895F06A2CC7837232C75BA52906DDA'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
-  silentArgs     = '/S'           # NSIS
+  silentArgs     = '/S'  # NSIS
 }
 
 Install-ChocolateyPackage @packageArgs

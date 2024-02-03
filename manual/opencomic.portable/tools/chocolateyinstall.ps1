8cbd2358b62af5d95282766611d6a2138f63c6fb
Aurocosh <Aurocosh@gmail.com>
2024-02-03 20:11:14 +0700

    Added portable version of OpenComic

---
+++ manual/opencomic.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$exeFile = Join-Path $toolsDir 'OpenComic.exe'
+
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+  fileFullPath = $exeFile
+  url          = 'https://github.com/ollm/OpenComic/releases/download/v1.0.0/OpenComic.Portable.1.0.0.exe'
+  checksum     = 'F179905B0D3D38820AAF1E35935812974C2FD39D678CC3A10F105EB741E2C989'
+  checksumType = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
+Install-ChocolateyShortcut -ShortcutFilePath "$env:ALLUSERSPROFILE\Desktop\OpenComic.lnk" -TargetPath $exeFile -IconLocation $exeFile
+Install-ChocolateyShortcut -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\OpenComic.lnk" -TargetPath $exeFile -IconLocation $exeFile
---
+++ manual/opencomic.portable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,2 @@
+﻿Remove-Item -Path "$env:ALLUSERSPROFILE\Desktop\OpenComic.lnk" -ErrorAction SilentlyContinue
+Remove-Item -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\OpenComic.lnk" -ErrorAction SilentlyContinue

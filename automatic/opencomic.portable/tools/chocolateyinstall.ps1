dc96893c8f145e262c785989c3f408ba14e43318
Aurocosh <Aurocosh@gmail.com>
2024-04-29 17:04:04 +0700

    Initial convertion of OpenComic.Portable to automated

--- automatic/opencomic.portable/tools/chocolateyinstall.ps1
+++ automatic/opencomic.portable/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $exeFile = Join-Path $toolsDir 'OpenComic.exe'
 
 $packageArgs = @{
-  packageName  = $env:ChocolateyPackageName
-  fileFullPath = $exeFile
-  url          = 'https://github.com/ollm/OpenComic/releases/download/v1.1.0/OpenComic.Portable.1.1.0.exe'
-  checksum     = 'E74E402B0F8938983E36FA446E56EA22F408CF92C7C715378CD4B178739D276C'
-  checksumType = 'sha256'
+  packageName    = $env:ChocolateyPackageName
+  fileFullPath   = $exeFile
+  url64bit       = 'https://github.com/ollm/OpenComic/releases/download/v1.1.0/OpenComic.Portable.1.1.0.exe'
+  checksum64     = 'E74E402B0F8938983E36FA446E56EA22F408CF92C7C715378CD4B178739D276C'
+  checksumType64 = 'sha256'
 }
 
 Get-ChocolateyWebFile @packageArgs

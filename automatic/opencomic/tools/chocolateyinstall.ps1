4e99d5f704ed3a8412c45037dfe611187e21f7a4
Aurocosh <Aurocosh@gmail.com>
2024-04-29 16:21:06 +0700

    Fixed OpenComic auto update

--- automatic/opencomic/tools/chocolateyinstall.ps1
+++ automatic/opencomic/tools/chocolateyinstall.ps1
@@ -1,12 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop' # stop on all errors
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/ollm/OpenComic/releases/download/v1.1.0/OpenComic.Setup.1.1.0.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
-  url64bit       = $url64
+  url64bit       = 'https://github.com/ollm/OpenComic/releases/download/v1.1.0/OpenComic.Setup.1.1.0.exe'
   softwareName   = 'opencomic*'
   checksum64     = 'ECAC622BF1D7795002EB940AE934B1065CA4FD922A92F00D088FC18C4C5FF3DE'
   checksumType64 = 'sha256'
--- automatic/opencomic/update.ps1
+++ automatic/opencomic/update.ps1
@@ -3,7 +3,7 @@ import-module au
 $releases = 'https://api.github.com/repos/ollm/OpenComic/releases/latest'
 
 function global:au_SearchReplace {
-   @{
+    @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
@@ -13,15 +13,15 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $download_assets = (Invoke-WebRequest $releases | ConvertFrom-Json).assets
-	$specific_asset = $download_assets | Where-Object name -like "OpenComic.Setup.*.exe"
-	$url64 = $specific_asset.browser_download_url
+    $specific_asset = $download_assets | Where-Object name -like "OpenComic.Setup.*.exe"
+    $url64 = $specific_asset.browser_download_url
 	
-	if ($url64 -match "\.(\d+\.\d+\.\d+)\.exe") {
-		$version = $matches[1]
-	}
+    if ($url64 -match "\.(\d+\.\d+\.\d+)\.exe") {
+        $version = $matches[1]
+    }
 	
     @{
-        URL64   = 'https://github.com' + $url64
+        URL64   = $url64
         Version = $version
     }
 }

932ae02c81fd034dc50659cf6b4213369408ccfa
Aurocosh <Aurocosh@gmail.com>
2024-07-08 19:05:06 +0700

    Updated Input Director to version 2.2

--- manual/InputDirector/tools/ChocolateyInstall.ps1
+++ manual/InputDirector/tools/ChocolateyInstall.ps1
@@ -1,3 +1,3 @@
 $drop = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage 'InputDirector' 'https://www.inputdirector.com/downloads/InputDirector.v2.1.1.zip' $drop -Checksum 6DBC57220B55B2D053CE9160D5E85170B6E6388CF75D0EF2384957DE4F180B33 -ChecksumType SHA256
-."$drop/InputDirector.v2.1.1.build150.Setup.exe" /S
+Install-ChocolateyZipPackage 'InputDirector' 'https://inputdirector.com/downloads/InputDirector.v2.2.zip' $drop -Checksum dd3ffab3cea386a421deaf54dbbf8c175efe0eef0f33e1e759331870e6e6ff5f -ChecksumType SHA256
+."$drop/InputDirector.v2.2.build164.Domain.Setup.exe" /S

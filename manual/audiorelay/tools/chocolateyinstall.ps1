76dc69e7dc42c3ad765bd48fe9c08c11e24636e6
Aurocosh <Aurocosh@gmail.com>
2024-07-11 21:15:07 +0700

    Updated silent args for AudioRelay

--- manual/audiorelay/tools/chocolateyinstall.ps1
+++ manual/audiorelay/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   checksum64     = 'ef4b2398d492a12ee25d25917b725294523b97bb6e532bed1a3e5ff020d6368f'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
-  silentArgs     = '/SILENT'
+  silentArgs     = '/VERYSILENT'
 }
 
 Install-ChocolateyPackage @packageArgs

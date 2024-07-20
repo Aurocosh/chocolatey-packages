3e99990dd0105c1b64780797226e22eb6dd797a9
Aurocosh <Aurocosh@gmail.com>
2024-07-20 17:02:37 +0700

    Covnerted AudioRelay into an automated package

--- automatic/InputDirector/update.ps1
+++ automatic/InputDirector/update.ps1
@@ -15,7 +15,7 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
     $re = "InputDirector\.v(\d+\.\d+(?:\.\d+)?)\.zip"
-    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $url = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
 	
     if ($url -match $re) {
         $version = $matches[1]
---
+++ automatic/audiorelay/update.ps1
@@ -0,0 +1,24 @@
+import-module au
+
+$downloadApi = 'https://api.audiorelay.net/downloads'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $response = Invoke-WebRequest -Uri $downloadApi -Method Get
+    $jsonValue = ConvertFrom-Json $response.Content
+	
+    @{
+        URL64   = $jsonValue.windows.downloadUrl
+        Version = $jsonValue.windows.version
+    }
+}
+
+update -ChecksumFor 64

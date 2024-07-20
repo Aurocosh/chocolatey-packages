c70a3f96aaae0d87faa1f6799afe65efb73c65d7
Aurocosh <Aurocosh@gmail.com>
2024-07-20 15:44:50 +0700

    Fixed auto update of InputDirector

--- automatic/InputDirector/update.ps1
+++ automatic/InputDirector/update.ps1
@@ -5,28 +5,28 @@ $releases = 'https://inputdirector.com/downloads.html'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"   = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
         }
     }
 }
 
 function global:au_GetLatest {
-	$download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases
 
-	$re  = "InputDirector\.v(\d+\.\d+(?:\.\d+)?)\.zip"
-	$url = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $re = "InputDirector\.v(\d+\.\d+(?:\.\d+)?)\.zip"
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
 	
-	if ($url -match $re) {
-		$version = $matches[1]
-	}
+    if ($url -match $re) {
+        $version = $matches[1]
+    }
 	
-	$url = 'https://inputdirector.com/' + $url
+    $url = 'https://inputdirector.com/' + $url
 
     @{
-        URL   = $url
+        URL     = $url
         Version = $version
     }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor 32 -NoCheckUrl

5a94d126d4130c135d709d821105b2ee968ddb56
Aurocosh <Aurocosh@gmail.com>
2024-07-20 22:52:59 +0700

    Added Houdoku.portable package

---
+++ automatic/houdoku.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packagePath = $(Split-Path -parent $toolsDir)
+$installPath = Join-Path $packagePath 'files'
+$exeFile = Join-Path $installPath 'Houdoku.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $installPath
+  url64bit       = 'https://github.com/xgi/houdoku/releases/download/v2.14.0/Houdoku-2.14.0-win.zip'
+  softwareName   = 'houdoku*'
+  checksum64     = '9466645245a5591ec60200be48af9c83c9997f76db8f5d26c27ffb525fba2d31'
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# $packageArgs.file = Join-Path -Path $toolsDir -ChildPath $installerName
+# Install-ChocolateyInstallPackage @packageArgs
+
+# Remove-Item "$toolsDir/$installerName"
+
+Install-ChocolateyShortcut -ShortcutFilePath "$env:ALLUSERSPROFILE\Desktop\Houdoku.lnk" -TargetPath $exeFile -IconLocation $exeFile
+Install-ChocolateyShortcut -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Houdoku.lnk" -TargetPath $exeFile -IconLocation $exeFile
---
+++ automatic/houdoku.portable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,2 @@
+﻿Remove-Item -Path "$env:ALLUSERSPROFILE\Desktop\Houdoku.lnk" -ErrorAction SilentlyContinue
+Remove-Item -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Houdoku.lnk" -ErrorAction SilentlyContinue

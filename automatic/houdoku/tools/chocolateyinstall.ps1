9f7c57f66f4ce4030e07c31941aef78ef6a67143
Aurocosh <Aurocosh@gmail.com>
2024-07-20 22:02:26 +0700

    Added Houdoku package

---
+++ automatic/houdoku/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url64bit       = 'https://github.com/xgi/houdoku/releases/download/v2.14.0/Houdoku-Setup-2.14.0.exe'
+  softwareName   = 'houdoku*'
+  checksum64     = 'eaeedada2f4580b503c24cad2daeb9e6ac33689e8764f95aff0120cf07333881'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'  # NSIS
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/houdoku/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+$releases = 'https://api.github.com/repos/xgi/houdoku/releases/latest'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_assets = (Invoke-WebRequest $releases | ConvertFrom-Json).assets
+    $specific_asset = $download_assets | Where-Object name -like "Houdoku-Setup-*.exe"
+    $url64 = $specific_asset.browser_download_url
+	
+    if ($url64 -match "-(\d+\.\d+\.\d+)\.exe") {
+        $version = $matches[1]
+    }
+	
+    @{
+        URL64   = $url64
+        Version = $version
+    }
+}
+
+update -ChecksumFor 64

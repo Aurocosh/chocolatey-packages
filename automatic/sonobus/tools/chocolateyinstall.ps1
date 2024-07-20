1a16ce1eb07bb17774f603b3c15108ea55c41852
Aurocosh <Aurocosh@gmail.com>
2024-07-20 17:59:27 +0700

    Added SonoBus package

---
+++ automatic/sonobus/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url64bit       = 'https://github.com/sonosaurus/sonobus/releases/download/1.7.2/sonobus-1.7.2-win.exe'
+  checksum64     = '9790d2dee5aee327dc4dbd69e43567ece833d41645a35f8adb05b3c39243cfd8'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/VERYSILENT'
+}
+
+Install-ChocolateyPackage @packageArgs

2e371c0c5eaac410802af9890d3326dd8d21ec40
Aurocosh <Aurocosh@gmail.com>
2024-07-21 13:46:17 +0700

    Added Lychee package

---
+++ automatic/lychee/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packagePath = $(Split-Path -parent $toolsDir)
+$exeFile = Join-Path $packagePath 'lychee.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileFullPath   = $exeFile
+  url64bit       = 'https://github.com/lycheeverse/lychee/releases/download/v0.15.1/lychee-v0.15.1-windows-x86_64.exe'
+  checksum64     = 'b75a5ba20d18fcd09b831451111d12501873ae7bdd5054819824f6e818e316f0'
+  checksumType64 = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs

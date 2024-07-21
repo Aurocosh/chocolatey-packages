2905226e6d4905be0dc70ef99fffc62951cfca49
Aurocosh <Aurocosh@gmail.com>
2024-07-21 23:10:22 +0700

    Removed nekoray package

--- automatic/nekoray/tools/chocolateyuninstall.ps1
+++
@@ -1,2 +0,0 @@
-﻿Remove-Item -Path "$env:ALLUSERSPROFILE\Desktop\Nekoray.lnk" -ErrorAction SilentlyContinue
-Remove-Item -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Nekoray.lnk" -ErrorAction SilentlyContinue

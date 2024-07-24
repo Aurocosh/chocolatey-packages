626be45a61c8681481cd771c95767efb173682e6
Aurocosh <Aurocosh@gmail.com>
2024-07-24 09:58:46 +0700

    Fixed trippy auto update

--- automatic/trippy/update.ps1
+++ automatic/trippy/update.ps1
@@ -6,11 +6,11 @@ import-module "$PSScriptRoot/../../_scripts/my_functions.psm1"
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(url64msvc\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
-            "(?i)(checksum64msvc\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+            "(?i)(url64msvc\s*=\s*)('.*')"      = "`$1'$($Latest.URL_MSVC)'"
+            "(?i)(checksum64msvc\s*=\s*)('.*')" = "`$1'$($Latest.CHECKSUM_MSVC)'"
 
-            "(?i)(url64gnu\s*=\s*)('.*')"       = "`$1'$($Latest.URL64)'"
-            "(?i)(checksum64gnu\s*=\s*)('.*')"  = "`$1'$($Latest.Checksum64)'"
+            "(?i)(url64gnu\s*=\s*)('.*')"       = "`$1'$($Latest.URL_GNU)'"
+            "(?i)(checksum64gnu\s*=\s*)('.*')"  = "`$1'$($Latest.CHECKSUM_GNU)'"
         }
     }
 }
@@ -21,11 +21,14 @@ function global:au_GetLatest {
         -RepoName trippy `
         -MainUrl32Regex "trippy-\d+.\d+.\d+-x86_64-pc-windows-msvc.zip" `
         -MainUrl64Regex "trippy-\d+.\d+.\d+-x86_64-pc-windows-gnu.zip"
+
     @{
-        URL     = $release.MainUrl32
-        URL64   = $release.MainUrl64
-        Version = $release.Version
+        URL_MSVC      = $release.MainUrl32
+        CHECKSUM_MSVC = (Get-RemoteChecksum $release.MainUrl32)
+        URL_GNU       = $release.MainUrl64
+        CHECKSUM_GNU  = (Get-RemoteChecksum $release.MainUrl64)
+        Version       = $release.Version
     }
 }
 
-update -ChecksumFor 32 64
+update -ChecksumFor none

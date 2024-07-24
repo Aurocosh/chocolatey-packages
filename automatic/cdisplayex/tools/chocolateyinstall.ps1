55115555bbf7c727703c39d30869c8547a86485d
Aurocosh <Aurocosh@gmail.com>
2024-07-24 23:00:45 +0700

    Added cdisplayex package

---
+++ automatic/cdisplayex/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  softwareName   = 'CDisplayEx*'
+  url            = 'https://www.cdisplayex.com/files/CDXWin32v1.10.33.exe'
+  checksum       = '3777afbc649901be6948bcd22f97f1c1d0f77c6375f5a027a88a62a75e5eecda'
+  checksumType   = 'sha256'
+  url64bit       = 'https://www.cdisplayex.com/files/CDXWin64v1.10.33.exe'
+  checksum64     = '38352a6240b3407b43c6b21d70d0ec5fca12592ab68b869cbfa9a8efc8d36cff'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/cdisplayex/update.ps1
@@ -0,0 +1,33 @@
+import-module au
+import-module "$PSScriptRoot/../../_scripts/my_functions.psm1"
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*urlt\s*=\s*)('.*')"       = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri 'https://www.cdisplayex.com/desktop/'
+
+    $regex32 = "CDXWin32v(\d+\.\d+\.\d+)\.exe"
+    $url32 = $download_page.links | Where-Object href -match $regex32 | Select-Object -First 1 -expand href
+
+    $regex64 = "CDXWin64v(\d+\.\d+\.\d+)\.exe"
+    $url64 = $download_page.links | Where-Object href -match $regex64 | Select-Object -First 1 -expand href
+
+    $version = $matches[1]
+
+    @{
+        URL32   = $url32
+        URL64   = $url64
+        Version = $version
+    }
+}
+
+update -ChecksumFor all

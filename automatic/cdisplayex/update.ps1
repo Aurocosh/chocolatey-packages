2b0f51196087753fbf2bb4e64b15bc44e1a236dd
Aurocosh <Aurocosh@gmail.com>
2024-07-24 23:21:13 +0700

    Fixed cdisplayex auto update

--- automatic/cdisplayex/update.ps1
+++ automatic/cdisplayex/update.ps1
@@ -4,7 +4,7 @@ import-module "$PSScriptRoot/../../_scripts/my_functions.psm1"
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*urlt\s*=\s*)('.*')"       = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
             "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"

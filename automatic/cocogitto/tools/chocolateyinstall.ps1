4ac3cf183f8948f5add81207fd75022f9c695e38
Aurocosh <Aurocosh@gmail.com>
2024-07-25 19:41:34 +0700

    Added cocogitto package

---
+++ automatic/cocogitto/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packagePath = $(Split-Path -parent $toolsDir)
+
+# Archive is .tar.gz, and it has to be extracted twice. So we cannot simply use Install-ChocolateyZipPackage to install the program
+
+$archiveFile = Join-Path $packagePath 'archive.tar.gz'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileFullPath   = $archiveFile
+  softwareName   = 'cog'
+  url64bit       = 'https://github.com/cocogitto/cocogitto/releases/download/6.1.0/cocogitto-6.1.0-x86_64-pc-windows-msvc.tar.gz'
+  checksum64     = 'a3887910a31eac6bc50cc751abdb00d89e5d4bc6152381763405a425ac025d0d'
+  checksumType64 = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
+
+Get-ChocolateyUnzip -FileFullPath $archiveFile -Destination $packagePath -DisableLogging
+Remove-Item -Path $archiveFile -Force
+
+$archiveFile = Join-Path $packagePath 'archive.tar'
+
+Get-ChocolateyUnzip -FileFullPath $archiveFile -Destination $packagePath -PackageName $env:ChocolateyPackageName
+Remove-Item -Path $archiveFile -Force

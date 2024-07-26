fb4ae1acbd7811557fe8b9838bce672be3247a9b
Aurocosh <Aurocosh@gmail.com>
2024-07-26 17:47:27 +0700

    Added fastflix package

---
+++ automatic/fastflix/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  softwareName   = 'FastFlix*'
+  url64bit       = 'https://github.com/cdgriffith/FastFlix/releases/download/5.7.4/FastFlix_5.7.4_installer.exe'
+  checksum64     = '3793d40f92c9be16932c9f5cd36990285781d15770df05d61064cc951af0f8a0'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'  # NSIS
+}
+
+Install-ChocolateyPackage @packageArgs

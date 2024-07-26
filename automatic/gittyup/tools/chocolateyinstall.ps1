57a7d179a209829e01e184126516a70981ba7202
Aurocosh <Aurocosh@gmail.com>
2024-07-26 22:00:11 +0700

    Addded gittyup package

---
+++ automatic/gittyup/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  softwareName   = 'Gittyup*'
+  url            = 'https://github.com/Murmele/Gittyup/releases/download/gittyup_v1.4.0/Gittyup-win32-1.4.0.exe'
+  checksum       = '0b1076c7ec7141d1c5b989d36c00011cd61e0fde646f59be208c9fbabe5eb836'
+  checksumType   = 'sha256'
+  url64bit       = 'https://github.com/Murmele/Gittyup/releases/download/gittyup_v1.4.0/Gittyup-win64-1.4.0.exe'
+  checksum64     = 'ab86d29dffd131718d2c2d0149ce90e6ad46e624e205e4feead011a800cd9302'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'  # NSIS
+}
+
+Install-ChocolateyPackage @packageArgs

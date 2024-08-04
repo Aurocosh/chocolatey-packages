059d3d47898b7b131e67123be25e0c7b56640ba3
Aurocosh <Aurocosh@gmail.com>
2024-08-04 14:43:31 +0700

    Updated xyplorer package

--- automatic/xyplorer/tools/chocolateyInstall.ps1
+++ automatic/xyplorer/tools/chocolateyInstall.ps1
@@ -1,43 +1,22 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$packageName = "xyplorer"
-$installerType = "EXE"
-$packageVersion = "26.30.0000"
-$url = "https://www.xyplorer.com/free-zer/26.30/xyplorer_full.zip"
-$silentArgs = "/S"
-$validExitCodes = @(0)
-$checksum = "2be73adfec1935381869f511f18c0649912d5bd9207a6c54aff8c0499f7d01ab"
-$checksumType = "sha256"
+$tempPath = Join-Path $env:temp $env:ChocolateyPackageName
 
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $tempPath
+  url            = 'https://www.xyplorer.com/free-zer/26.30/xyplorer_full.zip'
+  checksum       = '8da11cf86a80f0a53dcbd014230aff6ce3880b36578592b74d93431cc7dc2bbc'
+  checksumType   = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'  # NSIS
+  disableLogging = $true
+}
 
-#extract filename from source URL
-$filename = $url.Substring($url.LastIndexOf("/") + 1)
+Install-ChocolateyZipPackage @packageArgs
 
+$installerExe = (Get-ChildItem $tempPath -filter "XYplorer_*_Install.exe" -File | Select-Object -First 1).FullName
+$packageArgs.file = $installerExe
+Install-ChocolateyInstallPackage @packageArgs
 
-#establish temp folder path
-$tempPath = $env:temp, $packageName, $packageVersion -join "\"
-
-
-#establish full path to local copy of downloaded zip file
-$pathToZip = ($tempPath, $filename -join "\")
-
-
-# download zip package
-Get-ChocolateyWebFile  `
-    -PackageName $packageName `
-    -FileFullPath $pathToZip `
-    -Url $url `
-    -Checksum $checksum `
-    -ChecksumType $checksumType
-
-
-# extract it
-Get-ChocolateyUnzip $pathToZip $tempPath
-
-
-# establish path to extracted installer (exe)
-$pathToExe = Join-Path $tempPath (get-childitem $tempPath | where { $_.extension -eq ".exe" }).Name
-
-
-# install package
-Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$pathToExe" -validExitCodes $validExitCodes
+Remove-Item $tempPath -Recurse -Force -ErrorAction SilentlyContinue

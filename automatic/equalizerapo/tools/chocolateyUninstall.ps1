34a46ded0a0b0afe194ebde9298c1e2b198518c7
Aurocosh <Aurocosh@gmail.com>
2024-08-13 11:33:04 +0700

    Added equalizerapo package

---
+++ automatic/equalizerapo/tools/chocolateyUninstall.ps1
@@ -0,0 +1,8 @@
+$packageName = 'equalizerapo';
+$installerType = 'exe';
+$silentArgs = '/S';
+$validExitCodes = @(0);
+$packages = Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) -ErrorAction:SilentlyContinue;
+$packages | Where-Object { $_.DisplayName -like "$packageName*" } | ForEach-Object {
+    Uninstall-ChocolateyPackage -PackageName "$packageName" -FileType "$installerType" -SilentArgs "$($silentArgs)" -File "$($_.UninstallString)" -ValidExitCodes $validExitCodes;
+}

c5e9c58ad29c2f5ed422e8f1aa968fffad4a2e21
Aurocosh <Aurocosh@gmail.com>
2024-08-28 21:32:42 +0700

    Added breaktimer package

---
+++ automatic/breaktimer/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  softwareName   = 'BreakTimer*'
+  url64bit       = 'https://github.com/tom-james-watson/breaktimer-app/releases/download/v1.2.0/BreakTimer.exe'
+  checksum64     = 'fdadda85a982743f57f65c8a76a95b067d10c8c85f6fc8ac520cf84d8a86ad8f'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs

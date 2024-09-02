50e20c09b6c832c354cd444b49dcb2f674a6b883
Aurocosh <aurocosh@gmail.com>
2024-09-02 00:01:37 +0000

    AU: 1 updated - InputDirector [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/fe7dd835f33bf552cc98a751070391c40d1bf79b

--- automatic/InputDirector/tools/ChocolateyInstall.ps1
+++ automatic/InputDirector/tools/ChocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  url            = 'https://inputdirector.com/downloads/InputDirector.v2.2.zip'
-  checksum       = 'dd3ffab3cea386a421deaf54dbbf8c175efe0eef0f33e1e759331870e6e6ff5f'
+  url            = 'https://inputdirector.com/downloads/InputDirector.v2.3.zip'
+  checksum       = '51316c7993058fb7973f2a24523aabe7da66c08e89511486fc924f030c9af538'
   checksumType   = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/S'  # NSIS

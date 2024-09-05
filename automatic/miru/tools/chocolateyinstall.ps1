a6ed365702619c7e8c48a04b7fbf8a479d9edae8
Aurocosh <Aurocosh@gmail.com>
2024-09-05 19:10:40 +0700

    Added miru package

---
+++ automatic/miru/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  softwareName   = 'Miru*'
+  url64bit       = 'https://github.com/ThaUnknown/miru/releases/download/v5.5.6/win-Miru-5.5.6-installer.exe'
+  checksum64     = '330471973044d1ff265456fb532dc2c391780848277ba445648ee7e49829b469'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'  # NSIS
+}
+
+Install-ChocolateyPackage @packageArgs

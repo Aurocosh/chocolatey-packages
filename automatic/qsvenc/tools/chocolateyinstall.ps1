c584e92fd87afe4fbdce4b291681f085187bb090
Aurocosh <Aurocosh@gmail.com>
2024-09-18 19:59:59 +0700

    Added qsvenc package

---
+++ automatic/qsvenc/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packagePath = $(Split-Path -parent $toolsDir)
+$installPath = Join-Path $packagePath 'files'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $installPath
+  softwareName   = 'NVEnc*'
+  url            = 'https://github.com/rigaya/QSVEnc/releases/download/7.69/QSVEncC_7.69_Win32.7z'
+  checksum32     = 'eb2f67d4b3be346e811a6082043b12790363e2c31baee939c315f146930f6dd2'
+  checksumType32 = 'sha256'
+  url64bit       = 'https://github.com/rigaya/QSVEnc/releases/download/7.69/QSVEncC_7.69_x64.7z'
+  checksum64     = '320ff92ce5601e929a2d68a2450b6a68536b946bea9be1e7b02dc8010440fd85'
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$exePath64 = Join-Path $installPath 'QSVEncC64.exe'
+if (Test-Path $exePath64) {
+  Rename-Item -Path $exePath64 -NewName "QSVEncC.exe"
+}
---
+++ automatic/qsvenc/update.ps1
@@ -0,0 +1,28 @@
+import-module au
+import-module "$PSScriptRoot/../../_scripts/my_functions.psm1"
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $release = Get-LatestGithubRelease `
+        -GitUser rigaya `
+        -RepoName QSVEnc `
+        -MainUrl32Regex "QSVEncC_\d+\.\d+_Win32.7z" `
+        -MainUrl64Regex "QSVEncC_\d+\.\d+_x64.7z"
+    @{
+        URL32   = $release.MainUrl32
+        URL64   = $release.MainUrl64
+        Version = $release.Version
+    }
+}
+
+update -ChecksumFor all

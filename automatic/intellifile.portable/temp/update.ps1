5d7a70b9e11dfbb28b42ecf39401b2cb06a9ad11
Aurocosh <Aurocosh@gmail.com>
2024-09-18 14:15:20 +0700

    Fixed and disabled intellifile auto update

--- automatic/intellifile.portable/temp/update.ps1
+++ automatic/intellifile.portable/temp/update.ps1
@@ -14,7 +14,7 @@ function global:au_GetLatest {
     $release = Get-LatestGithubRelease `
         -GitUser mihaimoga `
         -RepoName IntelliFile `
-        -MainUrl64Regex "IntelliFile.zip "
+        -MainUrl64Regex "IntelliFile.zip"
     @{
         URL64   = $release.MainUrl64
         Version = $release.Version

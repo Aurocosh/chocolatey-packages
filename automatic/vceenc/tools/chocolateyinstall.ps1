2d843cefe885d56bf52b6108f99ca67554df8460
Aurocosh <Aurocosh@gmail.com>
2024-09-18 19:23:05 +0700

    Added vceenc package

---
+++ automatic/vceenc/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packagePath = $(Split-Path -parent $toolsDir)
+$installPath = Join-Path $packagePath 'files'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $installPath
+  softwareName   = 'VCEEnc*'
+  url            = 'https://github.com/rigaya/VCEEnc/releases/download/8.23/VCEEncC_8.23_Win32.7z'
+  checksum32     = '586623def06a99bc2d5a37af3d6c6d4803654b07f64760dade3532d1fb45e2ab'
+  checksumType32 = 'sha256'
+  url64bit       = 'https://github.com/rigaya/VCEEnc/releases/download/8.23/VCEEncC_8.23_x64.7z'
+  checksum64     = 'c11bd942c29aa6eb41979f8928945f8dd680fbaedb2f84e352624c2321eeef17'
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$exePath64 = Join-Path $installPath 'VCEEncC64.exe'
+if (Test-Path $exePath64) {
+  Rename-Item -Path $exePath64 -NewName "VCEEncC.exe"
+}
---
+++ automatic/vceenc/update.ps1
@@ -0,0 +1,28 @@
+import-module au
+import-module "$PSScriptRoot/../../_scripts/my_functions.psm1"
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $release = Get-LatestGithubRelease `
+        -GitUser rigaya `
+        -RepoName VCEEnc `
+        -MainUrl32Regex "VCEEncC_\d+\.\d+_Win32.7z" `
+        -MainUrl64Regex "VCEEncC_\d+\.\d+_x64.7z"
+    @{
+        URL32   = $release.MainUrl32
+        URL64   = $release.MainUrl64
+        Version = $release.Version
+    }
+}
+
+update -ChecksumFor all

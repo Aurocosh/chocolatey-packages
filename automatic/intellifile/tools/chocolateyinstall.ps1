101c26ee2a19944a4cffa92d0a85f043a990ca8e
Aurocosh <Aurocosh@gmail.com>
2024-09-18 13:46:01 +0700

    Added intellifile package

---
+++ automatic/intellifile/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'msi'
+  softwareName   = 'IntelliFile*'
+  url64bit       = 'https://github.com/mihaimoga/IntelliFile/releases/download/v1.33/IntelliFileSetup.msi'
+  checksum64     = '5c972ee68fe282fd54e98986e9b226b4eb3cb9c675ccf921409a5983afbef6ea'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/quiet'  # Windows Installer
+}
+
+Install-ChocolateyPackage @packageArgs

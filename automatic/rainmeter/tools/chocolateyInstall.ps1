97f59e608e3b89b1e06e89ee3f7b579bfce8bf17
Aurocosh <Aurocosh@gmail.com>
2024-09-22 16:23:13 +0700

    Fixed and refactored rainmeter package

--- automatic/rainmeter/tools/chocolateyInstall.ps1
+++ automatic/rainmeter/tools/chocolateyInstall.ps1
@@ -1,17 +1,14 @@
-$packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$ahkFile = "$toolsDir\button.ahk"
-
-Start-Process 'AutoHotkey' $ahkFile
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'EXE'
-  silentArgs    = ''
-  url           = $url
-  checksum 		= '{{checksum}}'
-  ChecksumType 	= 'sha256'
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  softwareName   = 'Miru*'
+  url64bit       = 'https://github.com/rainmeter/rainmeter/releases/download/v4.5.20.3803/Rainmeter-4.5.20.exe'
+  checksum64     = '9024b3b01b3883af3e12c3023ca9f7569893d25bb8154d785ac5737c7fff3ac9'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'  # NSIS
 }
 
 Install-ChocolateyPackage @packageArgs

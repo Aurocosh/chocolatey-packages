b4eb7cb69230cdf34c81b6ff873815a4131d1cb5
Aurocosh <Aurocosh@gmail.com>
2024-09-22 11:41:50 +0700

    Added missing dependency to equalizerapo

--- automatic/equalizerapo/tools/chocolateyInstall.ps1
+++ automatic/equalizerapo/tools/chocolateyInstall.ps1
@@ -1,20 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'equalizerapo'
-
 $packageArgs = @{
-  packageName    = $packageName
-  fileType       = 'EXE'
-  url            = 'https://sourceforge.net/projects/equalizerapo/files/1.4/EqualizerAPO32-1.4.exe/download'
-  url64bit       = 'https://sourceforge.net/projects/equalizerapo/files/1.4/EqualizerAPO64-1.4.exe/download'
-  silentArgs     = '/S'
-  validExitCodes = @(0)
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
   softwareName   = 'Equalizer APO*'
+  url            = 'https://sourceforge.net/projects/equalizerapo/files/1.4/EqualizerAPO32-1.4.exe/download'
   checksum       = '7da45cad07048eaaecd4fb2edcb7f534bef6035c63e3e20a6878816cf729ba4d'
   checksumType   = 'sha256'
+  url64bit       = 'https://sourceforge.net/projects/equalizerapo/files/1.4/EqualizerAPO64-1.4.exe/download'
   checksum64     = '77374fb48cda6e8739732672bbe6fed90e2e4a0ceed0a4e460f193135485fe50'
   checksumType64 = 'sha256'
-};
+  validExitCodes = @(0)
+  silentArgs     = '/S'
+}
 
 #Create monitor for "Configurator" ahead of actually installing the software
 $ScriptBlock = {

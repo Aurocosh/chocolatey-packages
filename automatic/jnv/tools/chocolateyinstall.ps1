57bda2094808e8d934d0a791572321b0f9dfee46
Aurocosh <Aurocosh@gmail.com>
2024-09-26 12:15:52 +0700

    Added jnv package

---
+++ automatic/jnv/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packagePath = $(Split-Path -parent $toolsDir)
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $packagePath
+  softwareName   = 'jnv'
+  url64bit       = 'https://github.com/ynqa/jnv/releases/download/v0.4.1/jnv-x86_64-pc-windows-msvc.zip'
+  checksum64     = 'fb44d9eb01fb29391f04d27f28ea9590adbb116c0b22c8816b8fc3801addcbbf'
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# $programExe = (Get-ChildItem $packagePath -filter "jnv.exe" -File -Recurse | Select-Object -First 1).FullName
+# Move-Item -Path $programExe -Destination $packagePath
+
+# $distPath = Join-Path $packagePath 'dist'
+# Remove-Item -Path $distPath -Force -Recurse
--- automatic/superfile/tools/chocolateyinstall.ps1
+++ automatic/superfile/tools/chocolateyinstall.ps1
@@ -8,7 +8,6 @@ $packageArgs = @{
   url64bit       = 'https://github.com/yorukot/superfile/releases/download/v1.1.4/superfile-windows-v1.1.4-amd64.zip'
   checksum64     = 'bc61cf9a3f7014ec32a7c435877029274a2fc08ab7a09035b1defb401c653717'
   checksumType64 = 'sha256'
-  specificFolder = "dist"
 }
 
 Install-ChocolateyZipPackage @packageArgs

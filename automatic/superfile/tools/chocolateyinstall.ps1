43bbf6b57badc2e4d54feb95d416d87125bd67a4
Aurocosh <Aurocosh@gmail.com>
2024-09-26 10:28:25 +0700

    Added superfile package

---
+++ automatic/superfile/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packagePath = $(Split-Path -parent $toolsDir)
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $packagePath
+  softwareName   = 'spf*'
+  url64bit       = 'https://github.com/yorukot/superfile/releases/download/v1.1.4/superfile-windows-v1.1.4-amd64.zip'
+  checksum64     = 'bc61cf9a3f7014ec32a7c435877029274a2fc08ab7a09035b1defb401c653717'
+  checksumType64 = 'sha256'
+  specificFolder = "dist"
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$programExe = (Get-ChildItem $packagePath -filter "spf.exe" -File -Recurse | Select-Object -First 1).FullName
+Move-Item -Path $programExe -Destination $packagePath
+
+$distPath = Join-Path $packagePath 'dist'
+Remove-Item -Path $distPath -Force -Recurse

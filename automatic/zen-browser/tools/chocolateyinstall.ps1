eefa2d555c0ca0ed69461452d535297feaebda47
Aurocosh <aurocosh@gmail.com>
2024-09-30 12:03:02 +0000

    AU: 1 updated - zen-browser [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/f6af5787676fc286344997954d40a6afaf4f6838

--- automatic/zen-browser/tools/chocolateyinstall.ps1
+++ automatic/zen-browser/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
-$urlOptimized64 = 'https://github.com/zen-browser/desktop/releases/download/1.0.1-a.5/zen.installer.exe'
-$checksumOptimized64 = '87173726b70d35149b1de0e6357557ddeeae5a97d31296bf6be56fe69412806b'
+$urlOptimized64 = 'https://github.com/zen-browser/desktop/releases/download/1.0.1-a.6/zen.installer.exe'
+$checksumOptimized64 = '30073077a83770dc3c13110d2499067343bc882fab51aef074479be492a40c3c'
 
-$urlGeneric64 = 'https://github.com/zen-browser/desktop/releases/download/1.0.1-a.5/zen.installer-generic.exe'
-$checksumGeneric64 = '7b642f075be6be613983d93ce9cfce95ec994ec9ab8cc8984991b90034fa3079'
+$urlGeneric64 = 'https://github.com/zen-browser/desktop/releases/download/1.0.1-a.6/zen.installer-generic.exe'
+$checksumGeneric64 = '06dafd606eaa54562de7c78fe28c197948a6b45796548ca3ca68b7f1e4039472'
 
 $PackageParameters = Get-PackageParameters
 

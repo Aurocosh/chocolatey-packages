5456b35c49c713b6742018dda212ed3b47e6de95
Aurocosh <aurocosh@gmail.com>
2024-10-07 00:02:29 +0000

    AU: 1 updated - topgrade [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/8281997042ec0dfb0fdd8c4383e9631c1d68f918

--- automatic/topgrade/tools/chocolateyinstall.ps1
+++ automatic/topgrade/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installPath
   softwareName   = 'topgrade*'
-  url64bit       = 'https://github.com/topgrade-rs/topgrade/releases/download/v15.0.0/topgrade-v15.0.0-x86_64-pc-windows-msvc.zip'
-  checksum64     = 'e2f271fb1b6361c785c7d311483ea77739d4d5bfe73b7b694e76a0a6d08ec7ce'
+  url64bit       = 'https://github.com/topgrade-rs/topgrade/releases/download/v16.0.0/topgrade-v16.0.0-x86_64-pc-windows-msvc.zip'
+  checksum64     = '8381e258ca289ba5d8488273cd965fc2b7e23e8baa4278fa7cc84566fc77cf11'
   checksumType64 = 'sha256'
 }
 

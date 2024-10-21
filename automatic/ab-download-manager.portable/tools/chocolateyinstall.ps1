81e7b59e5fbca39b9cbd70e5181a7eb37018cee6
Aurocosh <aurocosh@gmail.com>
2024-10-21 09:25:13 +0000

    AU: 2 updated - ab-download-manager ab-download-manager.portable [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/e3dd26249dd7fa47f61f06f44e88f714245e43fb

--- automatic/ab-download-manager.portable/tools/chocolateyinstall.ps1
+++ automatic/ab-download-manager.portable/tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'ABDownloadManager*'
-  url64bit       = 'https://github.com/amir1376/ab-download-manager/releases/download/v1.2.0/ABDownloadManager_1.2.0_windows.zip'
-  checksum64     = '895d4ab34a5d5afa04962c81b8df41f3e01560e0ba8aca0fa6bf3003089418a9'
+  url64bit       = 'https://github.com/amir1376/ab-download-manager/releases/download/v1.3.0/ABDownloadManager_1.3.0_windows.zip'
+  checksum64     = 'ae1ac372d873aca4ccf6f53b49f9657394d274dd06128e55c4377502033b54ac'
   checksumType64 = 'sha256'
 }
 

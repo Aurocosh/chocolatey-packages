ad6ae2856f3de8109780e3a07bb293442f0f5746
Aurocosh <aurocosh@gmail.com>
2024-11-10 00:02:48 +0000

    AU: 2 updated - breaktimer lime3ds [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/ec8b8c3aa3093eaa8175dceeaf431e5b7cf7f656

--- automatic/lime3ds/tools/chocolateyinstall.ps1
+++ automatic/lime3ds/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
-$url64msvc = 'https://github.com/Lime3DS/Lime3DS/releases/download/2119/lime3ds-2119-windows-msvc-installer.exe'
-$checksum64msvc = '1171bd0c114b828eaa34bde06f2c8ddb37947f05421703ed3bfef1f1836f9801'
+$url64msvc = 'https://github.com/Lime3DS/Lime3DS/releases/download/2119.1/lime3ds-2119.1-windows-msvc-installer.exe'
+$checksum64msvc = '020f500305362e07b2fa971b8628cd31b1be0bdde760c1e35158ed4b8c22d953'
 
-$url64msys2 = 'https://github.com/Lime3DS/Lime3DS/releases/download/2119/lime3ds-2119-windows-msys2-installer.exe'
-$checksum64msys2 = '55431481e69fafab69b7b749f9293226d4d97103e11fa232ec6bdc99ac0830cc'
+$url64msys2 = 'https://github.com/Lime3DS/Lime3DS/releases/download/2119.1/lime3ds-2119.1-windows-msys2-installer.exe'
+$checksum64msys2 = '50684e939766d197fe466bfe92c11bc28511e020eca93382c68fdde3b4ee9156'
 
 $PackageParameters = Get-PackageParameters
 

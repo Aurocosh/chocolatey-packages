87bb3dfe2b98fbf08003cc9cc4aa52e43aec2655
Aurocosh <Aurocosh@gmail.com>
2024-12-08 13:43:11 +0700

    proxy-checker: Added new package

---
+++ automatic/proxy-checker/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  softwareName   = 'Proxy Checker*'
+  url            = 'https://files.vovsoft.com/proxy-checker.exe?v=1.3'
+  checksum       = 'dda514ac8024e421cd1b234aadbba7bbbe9ed49b4f715e223b4aeb958a3d645d'
+  checksumType   = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/proxy-checker/update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"   = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri 'https://vovsoft.com/software/proxy-checker'
+
+    $versionRegex = "<b>Version:</b>\s*(\d+.\d+(?:.\d+)?)\s\("
+    if ($download_page.content -match $versionRegex) {
+        $version = $matches[1]
+        $url32 = "https://vovsoft.com/files/proxy-checker.exe?v=$version"
+    }
+	
+    @{
+        URL32   = $url32
+        Version = $version
+    }
+}
+
+update -ChecksumFor 32

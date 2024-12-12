c8c2d8616750cb05429109c5450ead4f6914eca6
Aurocosh <Aurocosh@gmail.com>
2024-12-12 20:39:13 +0700

    DotNet3.5: Fixed installation on Windows 11

--- manual/DotNet3.5/Tools/ChocolateyInstall.ps1
+++ manual/DotNet3.5/Tools/ChocolateyInstall.ps1
@@ -1,5 +1,5 @@
 if(-not (test-path "hklm:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
-  if((wmic os get caption | Out-String).Contains("Server")) {
+  if((Get-WmiObject -Class win32_operatingsystem).Caption.Contains("Server")) {
       $packageArgs = "/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3ServerFeatures"
       $statements = "cmd.exe $packageArgs"
       Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0, 1)
@@ -10,4 +10,4 @@ if(-not (test-path "hklm:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) {
 }
 else {
      Write-Host "Microsoft .Net 3.5 Framework is already installed on your machine."
- } 
+} 

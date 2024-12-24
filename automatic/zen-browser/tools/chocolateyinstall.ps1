4a9b2ffa1889ecd532941c7732f18a4a6b79146e
Aurocosh <Aurocosh@gmail.com>
2024-12-24 23:44:23 +0700

    zen-browser: Removed version selection

--- automatic/zen-browser/tools/chocolateyinstall.ps1
+++ automatic/zen-browser/tools/chocolateyinstall.ps1
@@ -1,28 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
-$urlOptimized64 = 'https://github.com/zen-browser/desktop/releases/download/1.0.2-b.3/zen.installer.exe'
-$checksumOptimized64 = 'ca10d32b545a23388503ad997e8f6dd2f6b75937ccc0e415d7b1dd0f9ec1b10b'
-
-$urlGeneric64 = 'https://github.com/zen-browser/desktop/releases/download/1.0.2-b.3/zen.installer-generic.exe'
-$checksumGeneric64 = '4a6b1bd8dd99cce3d69f644dff05b5de75e1582e523c94bfa710c6cb8305da4f'
-
-$PackageParameters = Get-PackageParameters
-
-if ($PackageParameters.Optimized) {
-  $url64 = $urlOptimized64
-  $checksum64 = $checksumOptimized64
-}
-else {
-  $url64 = $urlGeneric64
-  $checksum64 = $checksumGeneric64
-}
-
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'Zen Browser*'
-  url64bit       = $url64
-  checksum64     = $checksum64
+  url64bit       = 'https://github.com/zen-browser/desktop/releases/download/1.0.2-b.3/zen.installer.exe'
+  checksum64     = 'ca10d32b545a23388503ad997e8f6dd2f6b75937ccc0e415d7b1dd0f9ec1b10b'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/S'  # NSIS
--- automatic/zen-browser/update.ps1
+++ automatic/zen-browser/update.ps1
@@ -2,17 +2,10 @@ import-module au
 import-module "$PSScriptRoot/../../_scripts/my_functions.psm1"
 
 function global:au_SearchReplace {
-    $checksumGeneric = Get-RemoteChecksum $Latest.URL_GENERIC
-    $checksumOptimized = Get-RemoteChecksum $Latest.URL_OPTIMIZED
-
-    Write-Host "Checksum Generic: $checksumGeneric"
-    Write-Host "Checksum Optimized: $checksumOptimized"
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(urlGeneric64\s*=\s*)('.*')"        = "`$1'$($Latest.URL_GENERIC)'"
-            "(?i)(checksumGeneric64\s*=\s*)('.*')"   = "`$1'$checksumGeneric'"
-            "(?i)(urlOptimized64\s*=\s*)('.*')"      = "`$1'$($Latest.URL_OPTIMIZED)'"
-            "(?i)(checksumOptimized64\s*=\s*)('.*')" = "`$1'$checksumOptimized'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
@@ -21,13 +14,11 @@ function global:au_GetLatest {
     $release = Get-LatestGithubRelease `
         -GitUser zen-browser `
         -RepoName desktop `
-        -MainUrl32Regex "zen.installer-generic.exe" `
         -MainUrl64Regex "zen.installer.exe"
     @{
-        URL_GENERIC   = $release.MainUrl32
-        URL_OPTIMIZED = $release.MainUrl64
-        Version       = $release.Version
+        URL64   = $release.MainUrl64
+        Version = $release.Version
     }
 }
 
-update -ChecksumFor none
+update -ChecksumFor 64

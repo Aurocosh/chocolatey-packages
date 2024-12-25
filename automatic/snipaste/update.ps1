4bd6bb5d65327e5e89209bd0603b3fda7b830ebb
Aurocosh <Aurocosh@gmail.com>
2024-12-25 10:20:16 +0700

    snipaste: Removed commented text

--- automatic/snipaste/update.ps1
+++ automatic/snipaste/update.ps1
@@ -3,8 +3,6 @@ import-module au
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            # "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            # "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
             "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
@@ -19,17 +17,11 @@ function global:au_GetLatest {
         $version = $matches[1]
     }
 
-    # $re32 = "\/\/dl.snipaste.com\/win-x86(-beta)?"
-    # $url32 = $download_page.links | Where-Object href -match $re32 | Select-Object -First 1 -expand href
-    
     $re64 = "\/\/dl.snipaste.com\/win-x64(-beta)?"
     $url64 = $download_page.links | Where-Object href -match $re64 | Select-Object -First 1 -expand href
     
     $isBeta = $matches[1]
     
-    # if ($url32) {
-        # $url32 = "https:" + $url32
-    # }
     if ($url64) {
         $url64 = "https:" + $url64
     }
@@ -38,7 +30,6 @@ function global:au_GetLatest {
     }
     
     @{
-        # URL32   = $url32
         URL64   = $url64
         Version = $version
     }

c6a124f5dd178bc5fc75f188c859e75be825b509
Aurocosh <aurocosh@gmail.com>
2025-01-04 00:02:53 +0000

    AU: 1 updated - trippy [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/e86ec1b3cc8e3415d787ed8b314e5d552855275d

--- automatic/trippy/tools/chocolateyinstall.ps1
+++ automatic/trippy/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packagePath = $(Split-Path -parent $toolsDir)
 
-$url64msvc = 'https://github.com/fujiapple852/trippy/releases/download/0.12.1/trippy-0.12.1-x86_64-pc-windows-msvc.zip'
-$checksum64msvc = '0d288450f828585c114a14241c0be25710291e12913b7a0742cd69becc5c789d'
+$url64msvc = 'https://github.com/fujiapple852/trippy/releases/download/0.12.2/trippy-0.12.2-x86_64-pc-windows-msvc.zip'
+$checksum64msvc = 'a44a2de5d3f7536d0e5cc2f32665c8e39215dca5961889925c6048c318a169d8'
 
-$url64gnu = 'https://github.com/fujiapple852/trippy/releases/download/0.12.1/trippy-0.12.1-x86_64-pc-windows-gnu.zip'
-$checksum64gnu = '0bdb55bf03443671b8f5e2563041532f6c0a83dd6c5f124d0c850a817c7295f1'
+$url64gnu = 'https://github.com/fujiapple852/trippy/releases/download/0.12.2/trippy-0.12.2-x86_64-pc-windows-gnu.zip'
+$checksum64gnu = '5e8b801c4f0f3ce8baadd413790c2dcf9ef06d771d84cae7b310ca81d2ee690f'
 
 $PackageParameters = Get-PackageParameters
 

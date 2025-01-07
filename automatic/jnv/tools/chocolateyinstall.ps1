8a27039c09068f2c8631feeffb0f327d9afc9bf9
Aurocosh <aurocosh@gmail.com>
2025-01-07 00:02:28 +0000

    AU: 1 updated - jnv [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/d92e79c6453afd991e44992be8deb7666cc88c94

--- automatic/jnv/tools/chocolateyinstall.ps1
+++ automatic/jnv/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'jnv'
-  url64bit       = 'https://github.com/ynqa/jnv/releases/download/v0.4.2/jnv-x86_64-pc-windows-msvc.zip'
-  checksum64     = 'f702aadc6391a2a809080767089fb54ccb35264934d1ffdac554316dbabce1dd'
+  url64bit       = 'https://github.com/ynqa/jnv/releases/download/v0.5.0/jnv-x86_64-pc-windows-msvc.zip'
+  checksum64     = '71a1736348a2ddfe6af48c757125d4b55b567fbc123ce10652caeab6a9173acb'
   checksumType64 = 'sha256'
 }
 

cc9609836b3247628deaf84036a3cec03e342e0a
Aurocosh <aurocosh@gmail.com>
2025-01-21 00:02:48 +0000

    AU: 1 updated - dra [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/8a23c140a25f79067a959425b29993908546cf0e

--- automatic/dra/tools/chocolateyinstall.ps1
+++ automatic/dra/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'DRA*'
-  url64bit       = 'https://github.com/devmatteini/dra/releases/download/0.7.1/dra-0.7.1-x86_64-pc-windows-msvc.zip'
-  checksum64     = '749cad10a38a8035914d718ea95aa272da522c926e40079dd7a1cababc129315'
+  url64bit       = 'https://github.com/devmatteini/dra/releases/download/0.8.0/dra-0.8.0-x86_64-pc-windows-msvc.zip'
+  checksum64     = '211cfc6e8faf2d6ebc59297416b930643e9640ba6d1ecb3da55ea2b561d5dc05'
   checksumType64 = 'sha256'
 }
 

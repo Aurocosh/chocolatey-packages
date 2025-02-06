18252d4a6cd46a815191b1a3018a207297738027
Aurocosh <aurocosh@gmail.com>
2025-02-06 00:02:38 +0000

    AU: 1 updated - crip [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/0e0cbcddb08ca254bca4908432b032eb90971721

--- automatic/crip/tools/chocolateyinstall.ps1
+++ automatic/crip/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packagePath = $(Split-Path -parent $toolsDir)
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'crip*'
-  url64bit       = 'https://github.com/Hakky54/certificate-ripper/releases/download/2.3.0/crip-windows-amd64.zip'
-  checksum64     = '7f24ec0ac70d0008378b8a9fcea896fdbfcab33abb451995313e5541827bee37'
+  url64bit       = 'https://github.com/Hakky54/certificate-ripper/releases/download/2.4.0/crip-windows-amd64.zip'
+  checksum64     = '526c5fa48b4b02d7d3f43d8845f11897d18e27abfc21351b415a084e2ea83125'
   checksumType64 = 'sha256'
 }
 

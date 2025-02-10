4bb7333e8c74685b857244371dd8bd0d2199054a
Aurocosh <Aurocosh@gmail.com>
2025-02-10 23:08:00 +0700

    sourcegit: Fixed the checksum

--- automatic/sourcegit/tools/chocolateyinstall.ps1
+++ automatic/sourcegit/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   unzipLocation  = $packagePath
   softwareName   = 'SourceGit*'
   url64bit       = 'https://github.com/sourcegit-scm/sourcegit/releases/download/v2025.04/sourcegit_2025.04.win-x64.zip'
-  checksum64     = 'aa307a9f14924a5a536182c5894003a682496a768e41e59bb06181b71ed79c8b'
+  checksum64     = 'b4748e8b79ba93086e955aed65435c5efc1cdcb67baf3a4d17458be476ae1a3d'
   checksumType64 = 'sha256'
 }
 

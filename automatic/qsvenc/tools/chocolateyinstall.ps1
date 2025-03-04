3d10c7bffac8f9caec7325147a232a329640e6e6
Aurocosh <aurocosh@gmail.com>
2025-03-04 12:47:16 +0000

    AU: 3 updated - qsvenc rainmeter vceenc [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/5cc5fd391dfaa7f2b6d2881106c45761e6648992

--- automatic/qsvenc/tools/chocolateyinstall.ps1
+++ automatic/qsvenc/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installPath
   softwareName   = 'NVEnc*'
-  url            = 'https://github.com/rigaya/QSVEnc/releases/download/7.69/QSVEncC_7.69_Win32.7z'
-  checksum       = 'eb2f67d4b3be346e811a6082043b12790363e2c31baee939c315f146930f6dd2'
+  url            = 'https://github.com/rigaya/QSVEnc/releases/download/7.83/QSVEncC_7.83_Win32.7z'
+  checksum       = '867704b64c59948244213440e77c3303c2cf0bfbfc7ae8f12fc75eb0c7c693c6'
   checksumType   = 'sha256'
-  url64bit       = 'https://github.com/rigaya/QSVEnc/releases/download/7.69/QSVEncC_7.69_x64.7z'
-  checksum64     = '320ff92ce5601e929a2d68a2450b6a68536b946bea9be1e7b02dc8010440fd85'
+  url64bit       = 'https://github.com/rigaya/QSVEnc/releases/download/7.83/QSVEncC_7.83_x64.7z'
+  checksum64     = '4670d41a926133b28da2acbe05e8a02b0b0c0a66dc9e856be64db3e4a2ba4cdf'
   checksumType64 = 'sha256'
 }
 
--- automatic/vceenc/tools/chocolateyinstall.ps1
+++ automatic/vceenc/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installPath
   softwareName   = 'VCEEnc*'
-  url            = 'https://github.com/rigaya/VCEEnc/releases/download/8.23/VCEEncC_8.23_Win32.7z'
-  checksum       = '586623def06a99bc2d5a37af3d6c6d4803654b07f64760dade3532d1fb45e2ab'
+  url            = 'https://github.com/rigaya/VCEEnc/releases/download/8.32/VCEEncC_8.32_Win32.7z'
+  checksum       = '5b12ecac421865d8b6f590a6beb63d74b3717dd982f71a239304810082a73824'
   checksumType   = 'sha256'
-  url64bit       = 'https://github.com/rigaya/VCEEnc/releases/download/8.23/VCEEncC_8.23_x64.7z'
-  checksum64     = 'c11bd942c29aa6eb41979f8928945f8dd680fbaedb2f84e352624c2321eeef17'
+  url64bit       = 'https://github.com/rigaya/VCEEnc/releases/download/8.32/VCEEncC_8.32_x64.7z'
+  checksum64     = 'fe759d6c96a75fe5fc8cf7be94237fe85e267d1a81af05e279e94c8d69538df2'
   checksumType64 = 'sha256'
 }
 

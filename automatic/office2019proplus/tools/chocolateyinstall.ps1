2c5a6473fccd5b44c423287fb34a40238db6ac50
Aurocosh <aurocosh@gmail.com>
2025-03-07 12:02:23 +0000

    AU: 1 updated - office2019proplus [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/0554d6a4a157c659e372053ccb1b1ca30accf99c

--- automatic/office2019proplus/tools/chocolateyinstall.ps1
+++ automatic/office2019proplus/tools/chocolateyinstall.ps1
@@ -42,8 +42,8 @@ else {
 $packageArgs = @{
     packageName    = 'Office2019DeploymentTool'
     fileType       = 'exe'
-    url            = 'https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_18324-20194.exe'
-    checksum       = '4dc02c52340f2bd12ee6ed0069084710df6210b182842cbdae7bd43b79ae88af'
+    url            = 'https://download.microsoft.com/download/6c1eeb25-cf8b-41d9-8d0d-cc1dbc032140/officedeploymenttool_18526-20146.exe'
+    checksum       = '3d1936ecf7d89847f3c94132f828bfe0beca401a5b0877ab2e0253a8571e6508'
     checksumType   = 'sha256'
     softwareName   = 'Microsoft Office 2019 ProPlus*'
     silentArgs     = "/extract:`"$officetempfolder`" /log:`"$officetempfolder\OfficeInstall.log`" /quiet /norestart"

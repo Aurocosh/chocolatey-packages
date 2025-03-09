9c18db199b3cf31d56682299006c35c22c3fd2fe
Aurocosh <aurocosh@gmail.com>
2025-03-09 00:02:35 +0000

    AU: 2 updated - intellifile intellifile.portable [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/9123ea8bfeba65fb998526a7a0eaf759bb91828c

--- automatic/intellifile.portable/tools/chocolateyinstall.ps1
+++ automatic/intellifile.portable/tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'IntelliFile*'
-  url64bit       = 'https://github.com/mihaimoga/IntelliFile/releases/download/v1.36/IntelliFile.zip'
-  checksum64     = '4eb6789f18b90a4bf2253980fa7b6b5eed0a695b62a58b6943ca42ca1e48b8a3'
+  url64bit       = 'https://github.com/mihaimoga/IntelliFile/releases/download/v1.37/IntelliFile.zip'
+  checksum64     = 'f7a7bf1c91e686b4fcf5051fd502b32598e212e523bf8a7a441a8841fdcb5cf0'
   checksumType64 = 'sha256'
 }
 

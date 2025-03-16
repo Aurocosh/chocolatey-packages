595b064d816eb610e3b20cbda12205a2850df4ed
Aurocosh <Aurocosh@gmail.com>
2025-03-16 14:50:44 +0700

    azahar.portable: New package added

---
+++ automatic/azahar.portable/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,2 @@
+Get-Process -Name "azahar" -ErrorAction SilentlyContinue | Stop-Process -Force
+
---
+++ automatic/azahar.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packagePath = $(Split-Path -parent $toolsDir)
+# $installPath = Join-Path $packagePath 'files'
+# $exeFile = Join-Path $installPath 'azahar.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $packagePath
+  softwareName   = 'azahar*'
+  url64bit       = 'https://github.com/azahar-emu/azahar/releases/download/2120-rc1/azahar-2120-rc1-windows-msvc.zip'
+  checksum64     = '10e9b24c87ff4ac8549eb8d214a6dac14b46f59e37faf8ba42a032da5f480af2'
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$exeFile = (Get-ChildItem "$packagePath/azahar-*-windows-msvc/azahar.exe" -File | Select-Object -First 1).FullName
+
+Install-ChocolateyShortcut -ShortcutFilePath "$env:ALLUSERSPROFILE\Desktop\azahar.lnk" -TargetPath $exeFile -WorkingDirectory $packagePath
+Install-ChocolateyShortcut -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\azahar.lnk" -TargetPath $exeFile -WorkingDirectory $packagePath

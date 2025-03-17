4e11749d110028f66dbcea53a0870987d507affc
Aurocosh <Aurocosh@gmail.com>
2025-03-17 12:37:57 +0700

    r.studio: Refactoring

--- automatic/r.studio/tools/chocolateyInstall.ps1
+++ automatic/r.studio/tools/chocolateyInstall.ps1
@@ -1,11 +1,14 @@
-$packageArgs =@{
-  packageName       = 'R.Studio'
-  fileType          = 'EXE'
-  silentArgs        = '/S'
-  validExitCodes    = @(0)
-  url64bit          = 'https://download1.rstudio.org/electron/windows/RStudio-2023.12.1-402.exe'
-  checksumType64    = 'sha256'
-  checksum64        = 'd3c03c42a42c9b5cd4f3d72a0cfc0859f0099b8199af842da762b0584ab4bea0'
-  
+$ErrorActionPreference = 'Stop' # stop on all errors
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  softwareName   = 'RStudio*'
+  url64bit       = 'https://download1.rstudio.org/electron/windows/RStudio-2023.12.1-402.exe'
+  checksum64     = 'd3c03c42a42c9b5cd4f3d72a0cfc0859f0099b8199af842da762b0584ab4bea0'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'  # NSIS
 }
+
 Install-ChocolateyPackage @packageArgs

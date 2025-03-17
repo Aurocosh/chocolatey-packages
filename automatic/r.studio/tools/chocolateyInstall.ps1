60c7b61fa63e553125b6fdc71833daf8c9fd91b3
Aurocosh <aurocosh@gmail.com>
2025-03-17 06:21:36 +0000

    AU: 1 updated - r.studio [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/24f9cd37e9abf6a05f7c8fc0be2ab848061878e2

--- automatic/r.studio/tools/chocolateyInstall.ps1
+++ automatic/r.studio/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'RStudio*'
-  url64bit       = 'https://download1.rstudio.org/electron/windows/RStudio-2023.12.1-402.exe'
-  checksum64     = 'd3c03c42a42c9b5cd4f3d72a0cfc0859f0099b8199af842da762b0584ab4bea0'
+  url64bit       = 'https://download1.rstudio.org/electron/windows/RStudio-2024.12.1-563.exe'
+  checksum64     = 'bb369743052a5285640ff9dab713e332d4d1d2143a3fa774cd4703794eeaa54b'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/S'  # NSIS

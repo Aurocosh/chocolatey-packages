15f323b22e847b581c1f1f32d05405ec34aca1f2
Aurocosh <Aurocosh@gmail.com>
2025-03-17 14:59:12 +0700

    cherrytree: Fixed uninstall

--- automatic/cherrytree/tools/chocolateyinstall.ps1
+++ automatic/cherrytree/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  softwareName   = 'cherrytree*'
+  softwareName   = 'CherryTree*'
   url64bit       = 'https://www.giuspen.com/software/cherrytree_0.99.41.0_win64_setup.exe'
   checksum64     = '7c60e7c6aac949c9862833d92a84e9b76faa4c95f3d689f2448e16296e48e448'
   checksumType64 = 'sha256'

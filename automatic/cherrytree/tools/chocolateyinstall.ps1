951ad5a02e19fe0001f83d7884bbcf6c9de44f78
Aurocosh <Aurocosh@gmail.com>
2025-03-17 13:32:13 +0700

    cherrytree: Existing package added

---
+++ automatic/cherrytree/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName= 'cherrytree'
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.giuspen.com/software/cherrytree_0.99.41.0_win64_setup.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  url           = $url
+  silentArgs    = "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT"
+  softwareName  = 'Cherrytree*'
+  checksum      = '7c60e7c6aac949c9862833d92a84e9b76faa4c95f3d689f2448e16296e48e448'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs

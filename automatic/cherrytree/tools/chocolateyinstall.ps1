2d1634a3790f123a4b12dff1bbd07df3e062d072
Aurocosh <Aurocosh@gmail.com>
2025-03-17 14:03:02 +0700

    cherrytree: Fixed installation

--- automatic/cherrytree/tools/chocolateyinstall.ps1
+++ automatic/cherrytree/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   checksum64     = '7c60e7c6aac949c9862833d92a84e9b76faa4c95f3d689f2448e16296e48e448'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
-  silentArgs     = '/S'  # NSIS
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'  # Inno Setup
 }
 
 Install-ChocolateyPackage @packageArgs

3e2ace70d92a3be593b6aa74f7acec5752d08bbc
Aurocosh <Aurocosh@gmail.com>
2025-03-17 13:52:13 +0700

    cherrytree: Refactoring

--- automatic/cherrytree/tools/chocolateyinstall.ps1
+++ automatic/cherrytree/tools/chocolateyinstall.ps1
@@ -1,14 +1,15 @@
-﻿$packageName= 'cherrytree'
-$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.giuspen.com/software/cherrytree_0.99.41.0_win64_setup.exe'
+$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
-  packageName   = $packageName
-  url           = $url
-  silentArgs    = "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT"
-  softwareName  = 'Cherrytree*'
-  checksum      = '7c60e7c6aac949c9862833d92a84e9b76faa4c95f3d689f2448e16296e48e448'
-  checksumType  = 'sha256'
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  softwareName   = 'cherrytree*'
+  url64bit       = 'https://www.giuspen.com/software/cherrytree_0.99.41.0_win64_setup.exe'
+  checksum64     = '7c60e7c6aac949c9862833d92a84e9b76faa4c95f3d689f2448e16296e48e448'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'  # NSIS
 }
 
 Install-ChocolateyPackage @packageArgs
+

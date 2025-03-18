9d7ea2638db2e988a22247b63109e7f7680398e1
Aurocosh <aurocosh@gmail.com>
2025-03-18 00:02:39 +0000

    AU: 2 updated - azahar azahar.portable [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/3f27a387124ea33805ec40395116f9bea80bbaf7

--- automatic/azahar.portable/tools/chocolateyinstall.ps1
+++ automatic/azahar.portable/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packagePath = $(Split-Path -parent $toolsDir)
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'azahar*'
-  url64bit       = 'https://github.com/azahar-emu/azahar/releases/download/2120-rc1/azahar-2120-rc1-windows-msvc.zip'
-  checksum64     = '10e9b24c87ff4ac8549eb8d214a6dac14b46f59e37faf8ba42a032da5f480af2'
+  url64bit       = 'https://github.com/azahar-emu/azahar/releases/download/2120-rc3/azahar-2120-rc3-windows-msvc.zip'
+  checksum64     = 'bf55f09e3f06ca2b9080d3e420d0269a3c9c437785a8d3a4873574d5f82c22d8'
   checksumType64 = 'sha256'
 }
 
--- automatic/azahar/tools/chocolateyinstall.ps1
+++ automatic/azahar/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'azahar*'
-  url64bit       = 'https://github.com/azahar-emu/azahar/releases/download/2120-rc1/azahar-2120-rc1-windows-msvc-installer.exe'
-  checksum64     = 'c06cb2553fe1ffa691af5b00a0a7dd4bf209ebdc9ffb2202db726747a9af621a'
+  url64bit       = 'https://github.com/azahar-emu/azahar/releases/download/2120-rc3/azahar-2120-rc3-windows-msvc-installer.exe'
+  checksum64     = '29a13802d56f23c3945b95ac20a891a6439e8aea92843c8ef9fa6a24d3172d53'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/S'  # NSIS

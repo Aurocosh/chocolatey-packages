62c472ac4b5ed7887cb597180cb7c2f811579d5f
Aurocosh <aurocosh@gmail.com>
2025-04-12 00:03:11 +0000

    AU: 4 updated - intellifile intellifile.portable rainmeter zen-browser [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/782783812a427112dec619d228b3612506048461

--- automatic/intellifile.portable/tools/chocolateyinstall.ps1
+++ automatic/intellifile.portable/tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'IntelliFile*'
-  url64bit       = 'https://github.com/mihaimoga/IntelliFile/releases/download/v1.37/IntelliFile.zip'
-  checksum64     = 'f7a7bf1c91e686b4fcf5051fd502b32598e212e523bf8a7a441a8841fdcb5cf0'
+  url64bit       = 'https://github.com/mihaimoga/IntelliFile/releases/download/v1.38/IntelliFile.zip'
+  checksum64     = '982b25d02a77d81dac9c897e55d3c9f94f810f3039f24629434da5ce65793414'
   checksumType64 = 'sha256'
 }
 

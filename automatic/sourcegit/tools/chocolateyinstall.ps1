9a6835da8356cb4dc97aa71dd73737017ad4bcc3
Aurocosh <aurocosh@gmail.com>
2025-04-14 12:03:11 +0000

    AU: 1 updated - sourcegit [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/5a5afe19d5f8c8fe0dcb24be6eefac1312a2e96d

--- automatic/sourcegit/tools/chocolateyinstall.ps1
+++ automatic/sourcegit/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'SourceGit*'
-  url64bit       = 'https://github.com/sourcegit-scm/sourcegit/releases/download/v2025.12/sourcegit_2025.12.win-x64.zip'
-  checksum64     = '7e15bd9396d99936142aa46e6212c32e256a94a18e0006011e72be6c776a7e91'
+  url64bit       = 'https://github.com/sourcegit-scm/sourcegit/releases/download/v2025.13/sourcegit_2025.13.win-x64.zip'
+  checksum64     = '0fbb058c98fa19cd8e9b5ba6f0d304d8d85adc7c6aefa573d14e54c9e956d740'
   checksumType64 = 'sha256'
 }
 

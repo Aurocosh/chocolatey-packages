a1920b8e037728f11bc7bf5d0e804c1b4c95bae9
Aurocosh <aurocosh@gmail.com>
2025-04-29 12:02:29 +0000

    AU: 1 updated - qsvenc [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/21158315fc0782c9005ab33ccb84c72432d758fb

--- automatic/qsvenc/tools/chocolateyinstall.ps1
+++ automatic/qsvenc/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installPath
   softwareName   = 'NVEnc*'
-  url            = 'https://github.com/rigaya/QSVEnc/releases/download/7.88/QSVEncC_7.88_Win32.7z'
-  checksum       = '6a5c7a45f77357d33713ac418bd0622584c799d75a03d28e6b469ae63e493b16'
+  url            = 'https://github.com/rigaya/QSVEnc/releases/download/7.89/QSVEncC_7.89_Win32.7z'
+  checksum       = '1524d66b15f34a63965c881fa5ad925dbd3e71f7f4086c2f28dd4e3ea4326733'
   checksumType   = 'sha256'
-  url64bit       = 'https://github.com/rigaya/QSVEnc/releases/download/7.88/QSVEncC_7.88_x64.7z'
-  checksum64     = '57777e955bce37ea0c36119bdf43e743e75f7e3c767826d3b69daddc2abf5fff'
+  url64bit       = 'https://github.com/rigaya/QSVEnc/releases/download/7.89/QSVEncC_7.89_x64.7z'
+  checksum64     = '5f41893d194c34a81acd4bfe9eec3db3f0228df2a6d8c0e5f3aea39760e29b58'
   checksumType64 = 'sha256'
 }
 

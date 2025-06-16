9fd602c00bd8166c4134eb779da97fb9baf1da09
Aurocosh <Aurocosh@gmail.com>
2025-06-16 18:52:57 +0700

    dopamine3: Added new package

---
+++ automatic/dopamine3/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop' # stop on all errors
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  softwareName   = 'Dopamine*'
+  url64bit       = 'https://github.com/digimezzo/dopamine/releases/download/v3.0.0-preview.38/Dopamine-3.0.0-preview.38.exe'
+  checksum64     = 'd8251f2384f9707353bdf8aac847af922e735975c7b19b57301216f6a7f9d16a'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'  # NSIS
+}
+
+Install-ChocolateyPackage @packageArgs
+

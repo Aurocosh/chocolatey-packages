c83b91208526fe45c3300615cbc2b5257fc6f8b5
Aurocosh <aurocosh@gmail.com>
2025-06-19 12:03:18 +0000

    AU: 1 updated - vceenc [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/72bcee5956d8a6ec3a194a01030e2a84b9687676

--- automatic/vceenc/tools/chocolateyinstall.ps1
+++ automatic/vceenc/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installPath
   softwareName   = 'VCEEnc*'
-  url            = 'https://github.com/rigaya/VCEEnc/releases/download/8.35/VCEEncC_8.35_Win32.7z'
-  checksum       = '9fa82ead69a4a0a11e5725b7eeae6e5946bde5ecc3424554da91e70d87709d26'
+  url            = 'https://github.com/rigaya/VCEEnc/releases/download/8.36/VCEEncC_8.36_Win32.7z'
+  checksum       = 'e19addea3fab224a2e9b239fbf4e35aa2e87d5a9be6a501cb5416001c29e026a'
   checksumType   = 'sha256'
-  url64bit       = 'https://github.com/rigaya/VCEEnc/releases/download/8.35/VCEEncC_8.35_x64.7z'
-  checksum64     = '0c2f92b4be544170607e46f78884c3fc5c3bb702b4e50ee3420c18c6151ce150'
+  url64bit       = 'https://github.com/rigaya/VCEEnc/releases/download/8.36/VCEEncC_8.36_x64.7z'
+  checksum64     = '8e4200a984ccaec1d80804fc39f2e86039aa1d74820e5092ecc3378db7eb9555'
   checksumType64 = 'sha256'
 }
 

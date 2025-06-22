48d0657f1da4d358636e359fabb1117917fd7e4e
Aurocosh <aurocosh@gmail.com>
2025-06-22 00:03:05 +0000

    AU: 1 updated - dopamine3 [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/c2eef080ac317d28eafebfe798ebeacfe2d1d49f

--- automatic/dopamine3/tools/chocolateyinstall.ps1
+++ automatic/dopamine3/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'Dopamine*'
-  url64bit       = 'https://github.com/digimezzo/dopamine/releases/download/v3.0.0-preview.38/Dopamine-3.0.0-preview.38.exe'
-  checksum64     = 'd8251f2384f9707353bdf8aac847af922e735975c7b19b57301216f6a7f9d16a'
+  url64bit       = 'https://github.com/digimezzo/dopamine/releases/download/v3.0.0-preview.39/Dopamine-3.0.0-preview.39.exe'
+  checksum64     = 'f82d3e59f96098bc1dfef549e539252d40fa4c52777f9c5db9b6d142a16d83de'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/S'  # NSIS

f6a7e92790f498b8a24e9c4e26d69b8c79b29d68
Aurocosh <Aurocosh@gmail.com>
2025-06-27 10:12:25 +0700

    lutgen-rs: Fixed installation and auto update

--- automatic/lutgen-rs/tools/chocolateyinstall.ps1
+++ automatic/lutgen-rs/tools/chocolateyinstall.ps1
@@ -1,30 +1,14 @@
-﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packagePath = $(Split-Path -parent $toolsDir)
-
-$urlRegular64 = 'https://github.com/ozwaldorf/lutgen-rs/releases/download/v0.12.0/lutgen-0.12.0-x86_64-windows.zip'
-$checksumRegular64 = '3541f8fd3b4b9236aeec5f5d91b39ab6728e077d9855723e49788fb4caa31081'
-
-$urlLegacy64 = 'https://github.com/ozwaldorf/lutgen-rs/releases/download/v0.12.0/lutgen-0.12.0-x86_64-legacy-windows.zip'
-$checksumLegacy64 = '523dfc994668b2511a2b4a65fb012eec52e5b813467f7222be67aa7f752bf320'
-
-$PackageParameters = Get-PackageParameters
-
-if ($PackageParameters.Legacy) {
-  $url64 = $urlLegacy64
-  $checksum64 = $checksumLegacy64
-}
-else {
-  $url64 = $urlRegular64
-  $checksum64 = $checksumRegular64
-}
+$exeFile = Join-Path $packagePath 'lutgen-cli.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  unzipLocation  = $packagePath
-  softwareName   = 'lutgen*'
-  url64bit       = $url64
-  checksum64     = $checksum64
+  fileFullPath   = $exeFile
+  url64bit       = 'TODO'
+  checksum64     = 'TODO'
   checksumType64 = 'sha256'
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyWebFile @packageArgs
--- automatic/lutgen-rs/update.ps1
+++ automatic/lutgen-rs/update.ps1
@@ -2,17 +2,10 @@ import-module au
 import-module "$PSScriptRoot/../../_scripts/my_functions.psm1"
 
 function global:au_SearchReplace {
-    $checksumRegular = Get-RemoteChecksum $Latest.URL_REGULAR
-    $checksumLegacy = Get-RemoteChecksum $Latest.URL_LEGACY
-
-    Write-Host "Checksum Regular: $checksumRegular"
-    Write-Host "Checksum Legacy: $checksumLegacy"
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(urlRegular64\s*=\s*)('.*')"      = "`$1'$($Latest.URL_REGULAR)'"
-            "(?i)(checksumRegular64\s*=\s*)('.*')" = "`$1'$checksumRegular'"
-            "(?i)(urlLegacy64\s*=\s*)('.*')"       = "`$1'$($Latest.URL_LEGACY)'"
-            "(?i)(checksumLegacy64\s*=\s*)('.*')"  = "`$1'$checksumLegacy'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
@@ -21,13 +14,13 @@ function global:au_GetLatest {
     $release = Get-LatestGithubRelease `
         -GitUser ozwaldorf `
         -RepoName lutgen-rs `
-        -MainUrl32Regex "lutgen-\d+\.\d+\.\d+-x86_64-windows.zip" `
-        -MainUrl64Regex "lutgen-\d+\.\d+\.\d+-x86_64-legacy-windows.zip"
+        -MainUrl64Regex "lutgen-cli-v-x86_64-pc-windows-msvc.exe"
     @{
-        URL_REGULAR = $release.MainUrl32
-        URL_LEGACY  = $release.MainUrl64
-        Version     = $release.Version
+        URL64   = $release.MainUrl64
+        Version = $release.Version
     }
 }
 
-update -ChecksumFor none
+update -ChecksumFor 64
+
+Remove-Item -Path "$PSScriptRoot/lutgen-cli.exe" -Force -ErrorAction SilentlyContinue

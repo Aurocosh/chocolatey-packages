119da1708447560f45c64d9c68e5ea9c9dca65a9
Aurocosh <aurocosh@gmail.com>
2025-06-27 03:37:06 +0000

    AU: 1 updated - lutgen-rs [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/cef8ea7641e620d3adb458c4ee4b65d6daca3b99

--- automatic/lutgen-rs/tools/chocolateyinstall.ps1
+++ automatic/lutgen-rs/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $exeFile = Join-Path $packagePath 'lutgen-cli.exe'
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileFullPath   = $exeFile
-  url64bit       = 'TODO'
-  checksum64     = 'TODO'
+  url64bit       = 'https://github.com/ozwaldorf/lutgen-rs/releases/download/lutgen-v1.0.0/lutgen-cli-v-x86_64-pc-windows-msvc.exe'
+  checksum64     = '721cc8d1aeb65ff3506b26ec7097cd90ef9b735d972f0835eb0128da11f8eff7'
   checksumType64 = 'sha256'
 }
 

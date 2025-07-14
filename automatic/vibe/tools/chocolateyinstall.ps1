a29308e0a81d9b83dd41828081fe7889d80aefc1
Aurocosh <Aurocosh@gmail.com>
2025-07-14 21:08:56 +0700

    vibe: Added new package

---
+++ automatic/vibe/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop' # stop on all errors
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  softwareName   = 'vibe*'
+  url64bit       = 'https://github.com/thewh1teagle/vibe/releases/download/v3.0.5/vibe_3.0.5_x64-setup.exe'
+  checksum64     = 'd3ce37c6cf7ec791eeca789dd8b9689c29457631c86345bc5d1df28bbb67eaa1'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'  # NSIS
+}
+
+Install-ChocolateyPackage @packageArgs
+
---
+++ automatic/vibe/update.ps1
@@ -0,0 +1,25 @@
+import-module au
+import-module "$PSScriptRoot/../../_scripts/my_functions.psm1"
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $release = Get-LatestGithubRelease `
+        -GitUser thewh1teagle `
+        -RepoName vibe `
+        -MainUrl64Regex "vibe_\d+\.\d+\.\d+_x64-setup.exe"
+    @{
+        URL64   = $release.MainUrl64
+        Version = $release.Version
+    }
+}
+
+update -ChecksumFor 64
+

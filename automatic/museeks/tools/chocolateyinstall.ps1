d83a09f0caa88ec4c2ebedb3791e8105af95d0ec
Aurocosh <aurocosh@gmail.com>
2025-07-16 10:09:28 +0000

    AU: 2 updated - hayase museeks [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/b80c0bb1f35ee9d62806b29d52d765897be02f4e

--- automatic/museeks/tools/chocolateyinstall.ps1
+++ automatic/museeks/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'Museeks*'
-  url64bit       = 'https://github.com/martpie/museeks/releases/download/0.20.9/Museeks_0.20.9_x64-setup.exe'
-  checksum64     = '6dd9270c2d4e5c8a2712b0cbaf594c2956840bd2b0f050b10a247380351b75ac'
+  url64bit       = 'https://github.com/martpie/museeks/releases/download/0.22.3/Museeks_0.22.3_x64-setup.exe'
+  checksum64     = '8bab24a1297381e0f8e65098aa3221c57d9c3e3689d0538441395ea7221b0010'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/S'  # NSIS

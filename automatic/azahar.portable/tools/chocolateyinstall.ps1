46dcc064ccc72a80031dd22586428915de3371c6
Aurocosh <aurocosh@gmail.com>
2025-08-16 00:03:31 +0000

    AU: 3 updated - azahar azahar.portable office2019proplus [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/797ada65e15852d0d249b4ef0b794c5fccb296df

--- automatic/azahar.portable/tools/chocolateyinstall.ps1
+++ automatic/azahar.portable/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'azahar*'
-  url64bit       = 'https://github.com/azahar-emu/azahar/releases/download/2122.1/azahar-2122.1-windows-msvc.zip'
-  checksum64     = '2b168a2c1f3973b64e945a0f7341d97badddcab5e85600c476b18b21b31a40ee'
+  url64bit       = 'https://github.com/azahar-emu/azahar/releases/download/2123/azahar-2123-windows-msvc.zip'
+  checksum64     = 'a677a73dc2f4d562977c87a83bc30fde83e0e748986574368fd5461dcf501112'
   checksumType64 = 'sha256'
 }
 

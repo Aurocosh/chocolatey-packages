27d5b156e5f11ced57a5d61a2fd10f422d3d1233
Aurocosh <aurocosh@gmail.com>
2025-08-27 00:03:20 +0000

    AU: 2 updated - opencomic opencomic.portable [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/7a79519b0a70b225734a28629efbb6ebdb03381c

--- automatic/opencomic.portable/tools/chocolateyinstall.ps1
+++ automatic/opencomic.portable/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $exeFile = Join-Path $packagePath 'OpenComic.exe'
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileFullPath   = $exeFile
-  url64bit       = 'https://github.com/ollm/OpenComic/releases/download/v1.6.0/OpenComic.Portable.1.6.0.exe'
-  checksum64     = '457128d40c507dd0ef708a23f276e0208f73960eea4044c00884161c7bf3fe8f'
+  url64bit       = 'https://github.com/ollm/OpenComic/releases/download/v1.6.1/OpenComic.Portable.1.6.1.exe'
+  checksum64     = 'fb1b704f6659b1ac16415da2edbd3a83413b882fa34ccd53361affcfe642ed78'
   checksumType64 = 'sha256'
 }
 

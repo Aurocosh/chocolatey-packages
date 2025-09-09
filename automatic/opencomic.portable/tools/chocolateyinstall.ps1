cdf8661f8ac200cb88b8d9b8e623c003560546b3
Aurocosh <aurocosh@gmail.com>
2025-09-09 00:06:04 +0000

    AU: 2 updated - opencomic opencomic.portable [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/6c786d0bc72734ada56d2df107abe6fe43e9a2f6

--- automatic/opencomic.portable/tools/chocolateyinstall.ps1
+++ automatic/opencomic.portable/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $exeFile = Join-Path $packagePath 'OpenComic.exe'
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileFullPath   = $exeFile
-  url64bit       = 'https://github.com/ollm/OpenComic/releases/download/v1.6.1/OpenComic.Portable.1.6.1.exe'
-  checksum64     = 'fb1b704f6659b1ac16415da2edbd3a83413b882fa34ccd53361affcfe642ed78'
+  url64bit       = 'https://github.com/ollm/OpenComic/releases/download/v1.6.2/OpenComic.Portable.1.6.2.exe'
+  checksum64     = '948053c1f7329260139e623095dd1a23fa719592ff9b87c299481b6abea0c3ae'
   checksumType64 = 'sha256'
 }
 
--- automatic/opencomic/tools/chocolateyinstall.ps1
+++ automatic/opencomic/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url64bit       = 'https://github.com/ollm/OpenComic/releases/download/v1.6.1/OpenComic.Setup.1.6.1.exe'
+  url64bit       = 'https://github.com/ollm/OpenComic/releases/download/v1.6.2/OpenComic.Setup.1.6.2.exe'
   softwareName   = 'opencomic*'
-  checksum64     = 'f6f339d1153974e72dfb988b9a26599a59af7840fe2f269f1b3f2c7f7201be8e'
+  checksum64     = '990b599ebfea28054d5b7b9c4b3032f317457bf7ff88a6650dd0ceeffb420876'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/S'  # NSIS

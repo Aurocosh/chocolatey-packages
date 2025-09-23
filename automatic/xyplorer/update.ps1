ab4f27510691e2d5d737df92d8f48d20a9418b5b
Aurocosh <Aurocosh@gmail.com>
2025-09-23 08:14:10 +0700

    xyplorer: Fixed auto update

--- automatic/xyplorer/update.ps1
+++ automatic/xyplorer/update.ps1
@@ -3,8 +3,8 @@ Import-Module Chocolatey-AU
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.Url32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"      = "`$1'$($Latest.Url64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
@@ -12,20 +12,27 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $releases = 'https://www.xyplorer.com/freezer.php'
     $download_page = Invoke-WebRequest -Uri $releases
-    
+
     $re = "\?ver=((\d+\.\d+)\.\d+)"
     $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
     $version = $matches[1]
     $majorVersion = $matches[2]
-    
+
     if ($majorVersion) {
-        $url = "https://www.xyplorer.com/free-zer/$majorVersion/xyplorer_full.zip"
+        $url64 = "https://www.xyplorer.com/free-zer/$majorVersion/xyplorer64_full.zip"
+
+        $checksumUrl = "https://www.xyplorer.com/download/XYHash64-$version.txt"
+        $response = Invoke-WebRequest -Uri $checksumUrl
+
+        $response.Content -match "SHA-256\s*([a-fA-F0-9]{64})"
+        $checksum64 = $matches[1]
     }
 
     @{
-        Url32   = $url
+        Url64   = $url64
+        Checksum64  = $checksum64
         Version = $version
     }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none

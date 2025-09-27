a5e77795174cd31e7c9a2579bd49944ecfc5bee9
Aurocosh <Aurocosh@gmail.com>
2025-09-27 22:23:51 +0700

    tridnet: Fixed the shortcut

--- automatic/tridnet/tools/chocolateyInstall.ps1
+++ automatic/tridnet/tools/chocolateyInstall.ps1
@@ -2,8 +2,7 @@
 $ErrorActionPreference = 'Stop'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$packagePath = $(Split-Path -parent $toolsDir)
-$exeFile = Join-Path $packagePath 'TrIDNet.exe'
+$exeFile = Join-Path $toolsDir 'TrIDNet.exe'
 
 Install-ChocolateyShortcut -ShortcutFilePath "$env:ALLUSERSPROFILE\Desktop\TrIDNet.lnk" -TargetPath $exeFile -IconLocation $exeFile
 Install-ChocolateyShortcut -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\TrIDNet.lnk" -TargetPath $exeFile -IconLocation $exeFile

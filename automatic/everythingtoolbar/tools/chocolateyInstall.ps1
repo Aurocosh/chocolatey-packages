cc4904a5e4d2e5270f1354d093b8ac0dc7332047
Aurocosh <aurocosh@gmail.com>
2025-10-02 00:05:15 +0000

    AU: 2 updated - everythingtoolbar r.studio [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/c048caef9cef1d0f2b0feeaed4f5dc65841aebd3

--- automatic/everythingtoolbar/tools/chocolateyInstall.ps1
+++ automatic/everythingtoolbar/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'EverythingToolbar*'
-  url64bit       = 'https://github.com/srwi/EverythingToolbar/releases/download/2.0.4/EverythingToolbar-2.0.4-x64.exe'
-  checksum64     = '6353b2b4313718d4875f921d88c3de3f15191f7e9bcbfbc9bf52a02ac214fc55'
+  url64bit       = 'https://github.com/srwi/EverythingToolbar/releases/download/2.1.0/EverythingToolbar-2.1.0-x64.exe'
+  checksum64     = '17e5a9bffe1f5a9c0664f34d6fb5715e1b86892610b7bd65f544f0a595fcd871'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup

3b1a1c1cffadf4f00d12dd0bbf950be343a5e885
Aurocosh <Aurocosh@gmail.com>
2025-10-07 17:00:06 +0700

    glasswire: Existing package added

---
+++ automatic/glasswire/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'glasswire'
+  installerType  = 'exe'
+  url            = 'https://download.glasswire.com/f/glasswire-setup-3.5.821-full.exe'
+  checksum       = 'e1fbad7000249849828dac805541f2485bb43a3b4df17a788c96ed71f9291f94'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Glasswire*'
+  toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyPackage @packageArgs

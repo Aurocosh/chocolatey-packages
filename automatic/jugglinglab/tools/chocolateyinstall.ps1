06f078a5ebe6d61a28df235a179b94a397c36675
Aurocosh <aurocosh@gmail.com>
2025-10-17 12:05:55 +0000

    AU: 1 updated - jugglinglab [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/e35ad17f91b2dd10a73784f491e6b95489af72cb

--- automatic/jugglinglab/tools/chocolateyinstall.ps1
+++ automatic/jugglinglab/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'Juggling Lab*'
-  url            = 'https://storage.googleapis.com/jugglinglab-dl/JugglingLab-1.6.5.exe'
-  checksum       = 'a9ef1c00fe88915fc5ff598381559caf7d35af5d85396e29da99dc0355944422'
+  url            = 'https://storage.googleapis.com/jugglinglab-dl/JugglingLab-1.6.6.exe'
+  checksum       = 'baab3f9a889ff8d87b9d14367b7eb525f7d66d6d4fafdc5d0d45d1e672a4e265'
   checksumType   = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup

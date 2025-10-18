926533dfcd701069e09352bf9f0c3050af54faa0
Aurocosh <aurocosh@gmail.com>
2025-10-18 16:12:10 +0000

    AU: 1 updated - cocogitto [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/771055dab95b1a1018a8d89991ed6788ff8d0ee8

--- automatic/cocogitto/tools/chocolateyinstall.ps1
+++ automatic/cocogitto/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileFullPath   = $archiveFile
   softwareName   = 'cog'
-  url64bit       = 'https://github.com/cocogitto/cocogitto/releases/download/6.3.0/cocogitto-6.3.0-x86_64-pc-windows-msvc.tar.gz'
-  checksum64     = 'df66dc5cc1bc79c0b44ef2ef631824c59e00b00951c8add61d7c6b076ef907f2'
+  url64bit       = 'https://github.com/cocogitto/cocogitto/releases/download/6.4.0/cocogitto-6.4.0-x86_64-pc-windows-msvc.tar.gz'
+  checksum64     = 'b7a4a2457ce204f67ec9bb0995ac43a9a614c5e71e45e588452e38617e38c444'
   checksumType64 = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs

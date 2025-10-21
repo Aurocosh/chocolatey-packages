b2e179ed5fddfd4eb03d48aa471d3c6af186a0dd
Aurocosh <Aurocosh@gmail.com>
2025-10-21 21:47:43 +0700

    kicad: Fixed uninstallation

--- automatic/kicad/tools/chocolateyuninstall.ps1
+++ automatic/kicad/tools/chocolateyuninstall.ps1
@@ -1,33 +1,33 @@
-﻿$ErrorActionPreference = 'Stop';
-
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'KiCad*'
-  fileType      = 'EXE'
-  silentArgs   = '/S /allusers'
-  validExitCodes= @(0)
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'KiCad*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  fileType       = 'exe'
+  silentArgs     = '/S /allusers'
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
 }
 
 [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
 if ($key.Count -eq 1) {
-  $key | % {
-    $packageArgs['file'] = "$($_.UninstallString)"
-
-    if ($packageArgs['fileType'] -eq 'MSI') {
-      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
-
-      $packageArgs['file'] = ''
-    } else {
+  $key | ForEach-Object {
+    if($_.UninstallString -match '"([^"]+)"(?:\s*\/allusers|\s*\/s)') {
+      $packageArgs['file'] = $matches[1]
+    }
+    else {
+      $packageArgs['file'] = "$($_.UninstallString)" #NOTE: You may need to split this if it contains spaces, see below
     }
 
     Uninstall-ChocolateyPackage @packageArgs
   }
-} elseif ($key.Count -eq 0) {
+}
+elseif ($key.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
+}
+elseif ($key.Count -gt 1) {
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
+  $key | ForEach-Object { Write-Warning "- $($_.DisplayName)" }
 }
+

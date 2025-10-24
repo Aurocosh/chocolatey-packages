6a8bbb33a2e501c200edbb315656d031d96fed00
Aurocosh <Aurocosh@gmail.com>
2025-10-24 21:56:02 +0700

    buckets.install: Existing package added

---
+++ automatic/buckets.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+    PackageName     = $Env:ChocolateyPackageName
+    Url             = 'https://github.com/buckets/application/releases/download/v0.75.0/Buckets-Setup-0.75.0.exe'
+    Checksum        = '68D66AABB167E4A33121041987B75401837D8A0D5336F31F1A9CC2CF876D17A1'
+    ChecksumType    = 'sha256'
+    FileType        = 'exe'
+    silentArgs      = '/S'
+    validExitCodes  = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs

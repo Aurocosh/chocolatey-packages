13d95e48edae25a58a3b7b3456ad5e35f0346f2f
Aurocosh <Aurocosh@gmail.com>
2025-10-24 22:11:12 +0700

    buckets.install: Added auto update

---
+++ automatic/buckets.install/update.ps1
@@ -0,0 +1,32 @@
+Import-Module Chocolatey-AU
+Import-Module "$PSScriptRoot/../../_scripts/my_functions.psm1"
+
+$release = Get-LatestGithubRelease `
+    -GitUser buckets `
+    -RepoName application `
+    -MainUrl32Regex "Buckets-Setup-\d+\.\d+\.\d+.exe"
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+          "(?i)(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+		
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"   = "`$1'$($Latest.Url32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    @{
+        Url32        = $release.MainUrl32
+        Checksum32   = $release.MainUrl32_Sha256
+        Version      = $release.Version
+        ReleaseNotes = $release.ReleaseUrl
+    }
+}
+
+update -ChecksumFor $release.ChocoChecksumFor
+

d56f79b13f380967b89e3703bea55c3bf08ba931
Aurocosh <aurocosh@gmail.com>
2025-10-24 14:48:12 +0000

    AU: 2 updated - qsvenc tea [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/f28ecdde139a9b8e30023ee6a7136ed87c4970bd

--- automatic/qsvenc/tools/chocolateyinstall.ps1
+++ automatic/qsvenc/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installPath
   softwareName   = 'NVEnc*'
-  url            = 'https://github.com/rigaya/QSVEnc/releases/download/8.01/QSVEncC_8.01_Win32.7z'
-  checksum       = '299fa2c7e44ac9e3c8ceace40a382852c052ae87c6a16d4c626beb53460ec11a'
+  url            = 'https://github.com/rigaya/QSVEnc/releases/download/8.02/QSVEncC_8.02_Win32.7z'
+  checksum       = '94d86247c82ff59405ec66fbb2bdb3662251559f1f8fb5cb7310f5d21e32d8a0'
   checksumType   = 'sha256'
-  url64bit       = 'https://github.com/rigaya/QSVEnc/releases/download/8.01/QSVEncC_8.01_x64.7z'
-  checksum64     = '6a172f336ea82d68220c259713478b191544e305a492c88e6b1517a564122de3'
+  url64bit       = 'https://github.com/rigaya/QSVEnc/releases/download/8.02/QSVEncC_8.02_x64.7z'
+  checksum64     = '88f0b3e05583cff0a304b4b98f6fc99189315dad2fc919c32ab153f8e0a5f43e'
   checksumType64 = 'sha256'
 }
 
--- automatic/tea/tools/chocolateyinstall.ps1
+++ automatic/tea/tools/chocolateyinstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
   packageName    = $Env:ChocolateyPackageName
   fileFullPath   = $exeFile
   softwareName   = 'tea*'
-  url64bit       = 'https://gitea.com/gitea/tea/releases/download/v0.9.2/tea-0.9.2-windows-amd64.exe'
-  checksum64     = '7DBBF98468BC829D26D887058F41EA4505CFDF9C7BFFAA6495AAA30B6FE866B1'
+  url64bit       = 'https://gitea.com/gitea/tea/releases/download/v0.11.1/tea-0.11.1-windows-amd64.exe'
+  checksum64     = '02d15c5fd1a1ed1abfa4e1412ad7eb727b728ad0e71b429c8ec1bedf2547b962'
   checksumType64 = 'sha256'
 }
 

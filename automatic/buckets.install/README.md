2f8378dc786a109f74c2890ad738ff0de91eb2b7
Aurocosh <Aurocosh@gmail.com>
2025-10-24 22:10:51 +0700

    buckets.install: Converted the package

--- automatic/buckets.install/README.md
+++
@@ -1,9 +0,0 @@
-# <img src="https://rawcdn.githack.com/IndrekHaav/chocolatey-packages/1b8775481d616ad436c57e0117fce5d391ad8ac0/icons/buckets.png" width="48" height="48" /> [Buckets](https://www.budgetwithbuckets.com/)
-
-Buckets is a simple family budgeting app based on the envelope method. It is built with privacy in mind - your financial data stays on your computer and under your control. Sharing the budget between multiple devices, including smartphones, is supported, and there are multiple advanced features like macros, bank import and Amazon reconciliation.
-
-Buckets has an unlimited free trial and [a one-time license cost](https://www.budgetwithbuckets.com/buy/).
-
-## Documentation
-
-https://www.budgetwithbuckets.com/guide/
--- automatic/buckets.install/tools/chocolateyinstall.ps1
+++ automatic/buckets.install/tools/chocolateyinstall.ps1
@@ -1,13 +1,15 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
-    PackageName     = $Env:ChocolateyPackageName
-    Url             = 'https://github.com/buckets/application/releases/download/v0.75.0/Buckets-Setup-0.75.0.exe'
-    Checksum        = '68D66AABB167E4A33121041987B75401837D8A0D5336F31F1A9CC2CF876D17A1'
-    ChecksumType    = 'sha256'
-    FileType        = 'exe'
-    silentArgs      = '/S'
-    validExitCodes  = @(0)
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  softwareName   = 'application*'
+  url            = 'https://github.com/buckets/application/releases/download/v0.75.0/Buckets-Setup-0.75.0.exe'
+  checksum       = '68D66AABB167E4A33121041987B75401837D8A0D5336F31F1A9CC2CF876D17A1'
+  checksumType   = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'  # NSIS
 }
 
 Install-ChocolateyPackage @packageArgs
+

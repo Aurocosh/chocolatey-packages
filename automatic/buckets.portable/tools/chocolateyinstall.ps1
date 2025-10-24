549628c135b152786d12268392a093d457ebc3d0
Aurocosh <Aurocosh@gmail.com>
2025-10-24 22:40:29 +0700

    buckets.portable: Existing package added

---
+++ automatic/buckets.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    PackageName     = $Env:ChocolateyPackageName
+    Url             = 'https://github.com/buckets/application/releases/download/v0.75.0/Buckets-0.75.0.exe'
+    Checksum        = '7D456008196EEB2BCF29665A0C3CBE4DF9539CC37808F9F3170D85F12A3B0571'
+    ChecksumType    = 'sha256'
+    FileFullPath    = Join-Path $toolsDir 'buckets.exe'
+}
+
+Get-ChocolateyWebFile @packageArgs
---
+++ automatic/buckets.portable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+# Remove additional files created by the program
+$files = @('app.sqlite', 'log.log', 'state.json')
+$files | ForEach-Object {
+    Join-Path $toolsDir $_ | Remove-Item -ErrorAction SilentlyContinue
+}

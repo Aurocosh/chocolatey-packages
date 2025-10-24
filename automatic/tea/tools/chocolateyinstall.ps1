20ccbb697ac1c83496652568cdcc6b29c06080a7
Aurocosh <Aurocosh@gmail.com>
2025-10-24 21:31:54 +0700

    tea: Added auto update

--- automatic/tea/tools/chocolateyinstall.ps1
+++ automatic/tea/tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packagePath = $(Split-Path -parent $toolsDir)
+$exeFile = Join-Path $packagePath 'tea.exe'
 
 $packageArgs = @{
-    PackageName     = $Env:ChocolateyPackageName
-    Url             = 'https://gitea.com/gitea/tea/releases/download/v0.9.2/tea-0.9.2-windows-386.exe'
-    Checksum        = '89555C73716414CB8674BBDCFFE62151A02998B80BF1A54AAC68112551AF4707'
-    ChecksumType    = 'sha256'
-    Url64bit        = 'https://gitea.com/gitea/tea/releases/download/v0.9.2/tea-0.9.2-windows-amd64.exe'
-    Checksum64      = '7DBBF98468BC829D26D887058F41EA4505CFDF9C7BFFAA6495AAA30B6FE866B1'
-    ChecksumType64  = 'sha256'
-    FileFullPath    = Join-Path $toolsDir 'tea.exe'
+  packageName    = $Env:ChocolateyPackageName
+  fileFullPath   = $exeFile
+  softwareName   = 'tea*'
+  url64bit       = 'https://gitea.com/gitea/tea/releases/download/v0.9.2/tea-0.9.2-windows-amd64.exe'
+  checksum64     = '7DBBF98468BC829D26D887058F41EA4505CFDF9C7BFFAA6495AAA30B6FE866B1'
+  checksumType64 = 'sha256'
 }
 
 Get-ChocolateyWebFile @packageArgs
---
+++ automatic/tea/update.ps1
@@ -0,0 +1,31 @@
+Import-Module Chocolatey-AU
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+          "(?i)(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+		
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.Url64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $response = Invoke-WebRequest -Uri 'https://gitea.com/api/v1/repos/gitea/tea/releases/latest' -Method Get
+    $jsonValue = ConvertFrom-Json $response.Content
+
+    $regex64 = "tea-(\d+\.\d+\.\d+)-windows-amd64\.exe"
+    $url64 = $jsonValue.assets | Where-Object browser_download_url -match $regex64 | Select-Object -First 1 -expand href
+    $version = $matches[1]
+	
+    @{
+        Url64   = $url64
+        Version = $version
+        ReleaseNotes = $jsonValue.html_url
+    }
+}
+
+update -ChecksumFor 64

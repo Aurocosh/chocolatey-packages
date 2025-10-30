085034b4ba83fd5924094cd06fb516f526d07cc7
Aurocosh <aurocosh@gmail.com>
2025-10-30 03:14:21 +0000

    Updates unsuspended

---
+++ automatic/hayase/update.ps1
@@ -0,0 +1,26 @@
+Import-Module Chocolatey-AU
+Import-Module "$PSScriptRoot/../../_scripts/my_functions.psm1"
+
+$release = Get-LatestGithubRelease `
+    -GitUser ThaUnknown `
+    -RepoName miru `
+    -MainUrl64Regex "win-hayase-\d+\.\d+\.\d+-installer.exe"
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.Url64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    @{
+        Url64       = $release.MainUrl64
+        Checksum64  = $release.MainUrl64_Sha256
+        Version     = $release.Version
+    }
+}
+
+update -ChecksumFor $release.ChocoChecksumFor

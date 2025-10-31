2b2d113467be45535229f623d3492a64d69d6183
Aurocosh <Aurocosh@gmail.com>
2025-10-31 11:20:02 +0700

    r.studio: Added release notes

--- automatic/r.studio/update.ps1
+++ automatic/r.studio/update.ps1
@@ -6,6 +6,9 @@ function global:au_SearchReplace {
             "(?i)(^\s*url64bit\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum64)'"
         }
+        "$($Latest.PackageName).nuspec" = @{
+          "(?i)(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
     }
 }
 
@@ -15,10 +18,12 @@ function global:au_GetLatest {
     $regex64 = "RStudio-(\d+\.\d+\.\d+)-\d+\.exe"
     $url64 = $download_page.links | Where-Object href -match $regex64 | Select-Object -First 1 -expand href
     $version = $matches[1]
+    $releaseNotesUrl = "https://docs.posit.co/ide/news/#rstudio-$version"
 	
     @{
         URL64   = $url64
         Version = $version
+        ReleaseNotes = $releaseNotesUrl
     }
 }
 

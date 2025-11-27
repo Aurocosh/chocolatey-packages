08496c897c1c1520a9b1efcde8f59c05a7c2ac94
Aurocosh <aurocosh@gmail.com>
2025-11-27 12:09:20 +0000

    AU: 1 updated - nvenc [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/dfca6981fe314dee84fc1a2ff4faa4766c8ae2f0

--- automatic/nvenc/tools/chocolateyinstall.ps1
+++ automatic/nvenc/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installPath
   softwareName   = 'NVEnc*'
-  url            = 'https://github.com/rigaya/NVEnc/releases/download/9.07/NVEncC_9.07_Win32.7z'
-  checksum       = '226aa229b8f5a1cde7f0d9a6f99a2797a4f202a59e572b2ea2e1bb6d8784e5b8'
+  url            = 'https://github.com/rigaya/NVEnc/releases/download/9.08/NVEncC_9.08_Win32.7z'
+  checksum       = '598fdb623dc1145be2fc00a56e5728a323f72bd5ce9b68ce6b4eed651dfc64e1'
   checksumType   = 'sha256'
-  url64bit       = 'https://github.com/rigaya/NVEnc/releases/download/9.07/NVEncC_9.07_x64.7z'
-  checksum64     = 'e50ade48623164bbc94b101d7db9e84dbd29011e327bdfaabd50d036bc6a7c87'
+  url64bit       = 'https://github.com/rigaya/NVEnc/releases/download/9.08/NVEncC_9.08_x64.7z'
+  checksum64     = '76a0cd206a9d70d53271da81f74750db8fe02c60a3ed765343a14be5528dc48d'
   checksumType64 = 'sha256'
 }
 

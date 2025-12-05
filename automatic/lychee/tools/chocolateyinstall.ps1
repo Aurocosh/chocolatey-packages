a164d16425d3e6cedd1ec6407b2b22a0d2bd91f6
Aurocosh <aurocosh@gmail.com>
2025-12-05 12:05:48 +0000

    AU: 1 updated - lychee [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/c0f9886222a6c8ecdb4a19453b8a1f91f2aab2f1

--- automatic/lychee/tools/chocolateyinstall.ps1
+++ automatic/lychee/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $exeFile = Join-Path $packagePath 'lychee.exe'
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileFullPath   = $exeFile
-  url64bit       = 'https://github.com/lycheeverse/lychee/releases/download/lychee-v0.21.0/lychee-x86_64-windows.exe'
-  checksum64     = 'a1784c32c63ba46dccef0698ddf6be82a83a7d0455b0fd772423d601e3c70ab4'
+  url64bit       = 'https://github.com/lycheeverse/lychee/releases/download/lychee-v0.22.0/lychee-x86_64-windows.exe'
+  checksum64     = '3f416d1243e7b65e23547e5995b0e3c3388c1bc6451d36764acfd3ee0e5c8968'
   checksumType64 = 'sha256'
 }
 

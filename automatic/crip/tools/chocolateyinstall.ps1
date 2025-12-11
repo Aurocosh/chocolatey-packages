c08dfabb212d39a01a43951539be295b2fa50906
Aurocosh <Aurocosh@gmail.com>
2025-12-11 09:13:13 +0700

    crip: Fixed and updated

--- automatic/crip/tools/chocolateyinstall.ps1
+++ automatic/crip/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'crip*'
-  url64bit       = 'https://github.com/Hakky54/certificate-ripper/releases/download/v2.6.0/crip-windows-amd64.zip'
+  url64bit       = 'https://github.com/Hakky54/certificate-ripper/releases/download/2.6.0/crip-windows-amd64.zip'
   checksum64     = '479c33a877049d66f19febd606793e8c8635d3079ca3f4a9562f56b7efc1f742'
   checksumType64 = 'sha256'
 }

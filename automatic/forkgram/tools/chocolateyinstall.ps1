934d1c0d5423b741e9bf1222bc0030308ef56a98
Aurocosh <aurocosh@gmail.com>
2025-12-16 12:06:13 +0000

    AU: 2 updated - forkgram mediamonkey [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/e9e9cf970b88d94d08ce44f2683b52d8181aac83

--- automatic/forkgram/tools/chocolateyinstall.ps1
+++ automatic/forkgram/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installPath
   softwareName   = 'Telegram*'
-  url            = 'https://github.com/forkgram/tdesktop/releases/download/v6.3.6/Telegram_x86.zip'
-  checksum       = '54d5add59be0ed95a0d685a802ac79bf2d0bc07d446e31f2d3ce4386c11e243b'
+  url            = 'https://github.com/forkgram/tdesktop/releases/download/v6.3.9/Telegram_x86.zip'
+  checksum       = '4455002a1b8162fa3e4dd2fd619607c955766344fc697e7f946a9acfdaeaa60e'
   checksumType   = 'sha256'
-  url64bit       = 'https://github.com/forkgram/tdesktop/releases/download/v6.3.6/Telegram.zip'
-  checksum64     = '2977ad0fc6a3ca2c6b480f8be52854878633ae523aa2017dbbab44ab756251dd'
+  url64bit       = 'https://github.com/forkgram/tdesktop/releases/download/v6.3.9/Telegram.zip'
+  checksum64     = '0c13c0de5116beaa48832125db140fda59e0704c646721fa6d7e7a9203de0432'
   checksumType64 = 'sha256'
 }
 

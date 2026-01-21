9bfd45e545420a735ac0e0f897cba72972fc1c0d
Aurocosh <Aurocosh@gmail.com>
2026-01-21 21:44:37 +0700

    handy: New package added

---
+++ automatic/handy/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop' # stop on all errors
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  softwareName   = 'Handy*'
+  url64bit       = 'https://github.com/cjpais/Handy/releases/download/v0.7.0/Handy_0.7.0_x64-setup.exe'
+  checksum64     = '5cffdfffcb2f87b7622b88ded84adabd3e723aa4d345e9186ab94bf2ffd4e2c3'
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'  # NSIS
+}
+
+Install-ChocolateyPackage @packageArgs
+
---
+++ automatic/handy/update.ps1
@@ -0,0 +1,31 @@
+Import-Module Chocolatey-AU
+Import-Module "$PSScriptRoot/../../_scripts/my_functions.psm1"
+
+$release = Get-LatestGithubRelease `
+    -GitUser cjpais `
+    -RepoName Handy `
+    -MainUrl64Regex "Handy_\d+\.\d+\.\d+_x64-setup.exe"
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.Url64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+        "$($Latest.PackageName).nuspec" = @{
+          "(?i)(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    @{
+        Url64        = $release.MainUrl64
+        Checksum64   = $release.MainUrl64_Sha256
+        Version      = $release.Version
+        ReleaseNotes = $release.ReleaseUrl
+    }
+}
+
+update -ChecksumFor $release.ChocoChecksumFor
+

3c6fda4d97150ac2a28d5071ab63a7ecb90bfb3c
Aurocosh <aurocosh@gmail.com>
2026-03-02 00:05:57 +0000

    AU: 2 updated - handy trilium-notes [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/df4f471f72edfdea1769085c288fe6c2a55c50ca

--- automatic/trilium-notes/tools/chocolateyinstall.ps1
+++ automatic/trilium-notes/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packagePath = $(Split-Path -parent $toolsDir)
 $installPath = Join-Path $packagePath 'files'
 $exeFile = Join-Path $installPath 'trilium.exe'
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installPath
   softwareName   = 'trilium*'
-  url64bit       = 'https://github.com/TriliumNext/Trilium/releases/download/v0.101.3/TriliumNotes-v0.101.3-windows-x64.zip'
-  checksum64     = 'ae052d4fccd1b79ef8d20e697b9eca55c70778627fda6d6891cf1021e9d51ffb'
+  url64bit       = 'https://github.com/TriliumNext/Trilium/releases/download/v0.102.0/TriliumNotes-v0.102.0-windows-x64.zip'
+  checksum64     = 'b15617cd90b4fe2b4c56611b9938636baa87b2c7de1475fba9f22917bfb8a7b9'
   checksumType64 = 'sha256'
 }
 

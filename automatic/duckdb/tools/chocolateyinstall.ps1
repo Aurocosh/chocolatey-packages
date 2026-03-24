ccf92feb606e08151b0335e4d5cf07d9b1a3b28a
Aurocosh <aurocosh@gmail.com>
2026-03-24 00:06:45 +0000

    AU: 2 updated - duckdb tridnet [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/0c715121ff8691afb2ede6621d266540d17d3c83

--- automatic/duckdb/tools/chocolateyinstall.ps1
+++ automatic/duckdb/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installPath
   softwareName   = 'duckdb*'
-  url64bit       = 'https://github.com/duckdb/duckdb/releases/download/v1.5.0/duckdb_cli-windows-amd64.zip'
-  checksum64     = '90d5187208939cffce62736c7293d7efb25aed057c23afb5a17296c1a2c236de'
+  url64bit       = 'https://github.com/duckdb/duckdb/releases/download/v1.5.1/duckdb_cli-windows-amd64.zip'
+  checksum64     = '22cc4b9dcffe6b01300d2e78b5918a13cf4335305fec854a9277db3bbf2e4a83'
   checksumType64 = 'sha256'
 }
 

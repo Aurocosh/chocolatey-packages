a3d91f8c3176d30a8eb75a6e123bc33cd2d6f4d9
Aurocosh <Aurocosh@gmail.com>
2026-04-03 23:00:05 +0700

    kicad: Adjusted auto update script

--- automatic/kicad/update.ps1
+++ automatic/kicad/update.ps1
@@ -4,7 +4,7 @@ Import-Module "$PSScriptRoot/../../_scripts/my_functions.psm1"
 $release = Get-LatestGithubRelease `
     -GitUser KiCad `
     -RepoName kicad-source-mirror `
-    -MainUrl64Regex "kicad-\d+\.\d+\.\d+-x86_64.exe"
+    -MainUrl64Regex "kicad-\d+\.\d+\.\d+(?:-\d+)?-x86_64.exe"
 
 function global:au_SearchReplace {
     @{

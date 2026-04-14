18a3e5f02a6c3783889afcfd97a5d61c64d46dba
Aurocosh <aurocosh@gmail.com>
2026-04-14 12:06:05 +0000

    AU: 2 updated - wizfile wizfile.portable [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/4f5f224e0f8858381af5a1ef62b1d7c0abf1b409

--- automatic/wizfile.portable/tools/chocolateyinstall.ps1
+++ automatic/wizfile.portable/tools/chocolateyinstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'WizFile*'
-  url64bit       = 'https://antibody-software.com/files/wizfile_3_13_portable.zip'
-  checksum64     = '7fe2d518eed0c1fc95252f1096fcc63ecd26e89171bd1b5517ae197c822bd0ef'
+  url64bit       = 'https://antibody-software.com/files/wizfile_3_14_portable.zip'
+  checksum64     = '51cc0567f908be6a765ac281b4e3159aeb85a89d3a3f7229fad7883cc1846048'
   checksumType64 = 'sha256'
 }
 

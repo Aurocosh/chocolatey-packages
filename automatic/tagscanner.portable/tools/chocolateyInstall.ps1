9ead3001a033d3ea10991261f4009e7cb2c52435
Aurocosh <aurocosh@gmail.com>
2026-05-20 00:05:00 +0000

    AU: 1 updated - tagscanner.portable [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/1edee0465f6268be75320d163774856cc4fc2b7a

--- automatic/tagscanner.portable/tools/chocolateyInstall.ps1
+++ automatic/tagscanner.portable/tools/chocolateyInstall.ps1
@@ -10,11 +10,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installPath
   softwareName   = 'Tagscan*'
-  url            = 'https://www.xdlab.ru/files/tagscan-6.1.20.zip'
-  checksum       = '1bb4bf042c2f5cf4316a089cb754e81c3dac82253c1a3f6510b64e8b786b917f'
+  url            = 'https://www.xdlab.ru/files/tagscan-6.1.21.zip'
+  checksum       = 'b542a10e358877ef19be2a72f3a7bfde7d4daefb7c3f00ce13b71f324f2edf37'
   checksumType   = 'sha256'
-  url64bit       = 'https://www.xdlab.ru/files/tagscan-6.1.20_x64.zip'
-  checksum64     = '888a8f228a36accf2f7793b991c0260d36cea5a3489405431b7a051c7fcd474d'
+  url64bit       = 'https://www.xdlab.ru/files/tagscan-6.1.21_x64.zip'
+  checksum64     = 'e71b537f55eadf8add4f2e369e19ea2cea63e60af5667d9725121dad390474fe'
   checksumType64 = 'sha256'
 }
 

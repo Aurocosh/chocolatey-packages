3427b84d47bc3dab14a0efe36204b9e32509fca0
Aurocosh <aurocosh@gmail.com>
2026-05-22 00:06:34 +0000

    AU: 2 updated - adguardhome tridnet [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/9e74f7e2ae1a125ab3ac0eef4e68beb6c13f445b

--- automatic/adguardhome/tools/chocolateyInstall.ps1
+++ automatic/adguardhome/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'AdGuardHome*'
-  url            = 'https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.75/AdGuardHome_windows_386.zip'
-  checksum       = '79abba3eb903675b86bd2d8ac2eb01c65bdc116caca27f7217edf6b4dcf9c690'
+  url            = 'https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.76/AdGuardHome_windows_386.zip'
+  checksum       = 'c3ad01d1eef8e1e28168c6e0059e8c94253bb4ce8fbd4601c11918b8b8f3f93c'
   checksumType   = 'sha256'
-  url64bit       = 'https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.75/AdGuardHome_windows_amd64.zip'
-  checksum64     = 'ae01eb4b70786cd273f3925b83e0b9c25b296d5ef5ca173e0337e0a8d7d94432'
+  url64bit       = 'https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.76/AdGuardHome_windows_amd64.zip'
+  checksum64     = '80748442082f07dfdf9c1cdaeeb88e522e99afcdf1aedca3848338ce42c23ee6'
   checksumType64 = 'sha256'
 }
 

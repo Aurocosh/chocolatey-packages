2f96fa1656f94e1a279a7b8efe45c38303c891a2
Aurocosh <Aurocosh@gmail.com>
2026-06-26 22:03:05 +0700

    DotNet3.5: Fixed installation on new Windows versions

---
+++ automatic/DotNet3.5/tools/chocolateyinstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+
+if (Test-Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5') {
+  Write-Host 'Microsoft .Net 3.5 Framework is already installed on your machine.'
+  return
+}
+
+$build = [int](Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').CurrentBuild
+
+if ($build -ge 28000) {
+  $packageArgs = @{
+    packageName      = $env:ChocolateyPackageName
+    fileType         = 'exe'
+    softwareName     = 'Microsoft .NET Framework 3.5*'
+    url64bit         = 'https://go.microsoft.com/fwlink/?LinkID=2337635'
+    checksum64       = 'b969d1d0a662bdfd20f42aed9f0ede21122723fbbcee545f16da7150ff01926d'
+    checksumType64   = 'sha256'
+    validExitCodes   = @(0, 3010, 1641)
+    silentArgs       = '/q /norestart'
+  }
+  Install-ChocolateyPackage @packageArgs
+}
+else {
+  if ((Get-WmiObject -Class win32_operatingsystem).Caption.Contains('Server')) {
+    $packageArgs = '/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3ServerFeatures'
+    $statements = "cmd.exe $packageArgs"
+    Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0, 1)
+  }
+  $packageArgs = '/c DISM /Online /NoRestart /Enable-Feature /FeatureName:NetFx3'
+  $statements = "cmd.exe $packageArgs"
+  Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0)
+}
---
+++ automatic/DotNet3.5/update.ps1
@@ -0,0 +1,50 @@
+Import-Module Chocolatey-AU
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.Url64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+        ".\DotNet3.5.nuspec" = @{
+            "(?i)(^\s*<releaseNotes>)[^<]*(</releaseNotes>)`$" = "`${1}$($Latest.ReleaseNotes)`${2}"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $downloadPage = 'https://dotnet.microsoft.com/en-us/download/dotnet-framework/net35-sp1'
+    $response = Invoke-WebRequest -Uri $downloadPage -UseBasicParsing
+
+    $link = $response.Links | Where-Object {
+        $_.'data-bi-dlnm' -eq '.NET Framework 3.5 Standalone' -and
+        $_.'data-bi-name' -eq 'Download' -and
+        $_.'data-bi-id' -eq 'download'
+    } | Select-Object -First 1
+
+    if (-not $link) {
+        throw "Standalone .NET Framework 3.5 download link not found on $downloadPage"
+    }
+
+    $url = $link.href
+
+    $installScriptPath = "$PSScriptRoot/tools/chocolateyinstall.ps1"
+    (Get-Content -Path $installScriptPath -Raw) -match "(?i)url64bit\s*=\s*'([^']*)'"
+    $currentUrl = $matches[1]
+
+    $nuspecFile = "$PSScriptRoot/DotNet3.5.nuspec"
+    (Get-Content -Path $nuspecFile -Raw) -match '<version>([^<]*)<\/version>'
+    $version = $matches[1]
+
+    if ($url -and ($url -ne $currentUrl)) {
+        $version = '1.0.' + (Get-Date).ToString('yyyyMMdd')
+    }
+
+    @{
+        Url64        = $url
+        Version      = $version
+        ReleaseNotes = $downloadPage
+    }
+}
+
+update -ChecksumFor 64

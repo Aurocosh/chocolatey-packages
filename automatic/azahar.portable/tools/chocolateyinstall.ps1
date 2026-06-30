a7b825f164db641060a0d98b10ce667d2fa3b201
Aurocosh <aurocosh@gmail.com>
2026-06-30 00:05:22 +0000

    AU: 3 updated - azahar azahar.portable r.studio [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/655e4bb259b25b487e62b4eac6e026617b1ce531

--- automatic/azahar.portable/tools/chocolateyinstall.ps1
+++ automatic/azahar.portable/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'azahar*'
-  url64bit       = 'https://github.com/azahar-emu/azahar/releases/download/2125.1.2/azahar-windows-msvc-2125.1.2.zip'
-  checksum64     = '474a0036098992841ccd276a2c981fa7b3afb14e9951f9fc62c4a26063bbfee5'
+  url64bit       = 'https://github.com/azahar-emu/azahar/releases/download/2125.1.3/azahar-windows-msvc-2125.1.3.zip'
+  checksum64     = '7d5fbc8de1364998b15c96437d090b88690653e17717b60d06d0de54c42522da'
   checksumType64 = 'sha256'
 }
 

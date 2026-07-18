e71bd3d1f2719d5b0fddd8753f64d5c892792669
Aurocosh <aurocosh@gmail.com>
2026-07-18 00:05:04 +0000

    AU: 2 updated - libation-chardonnay libation-classic [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/3df2693dbad1df618c6a2941458a4c1839ad64ab

--- automatic/libation-chardonnay/tools/chocolateyinstall.ps1
+++ automatic/libation-chardonnay/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'Libation (Chardonnay)'
-  url64bit       = 'https://github.com/rmcrackan/Libation/releases/download/v13.5.0/Libation.13.5.0-windows-chardonnay-x64-setup.exe'
-  checksum64     = 'd8db3cb7de4d938e9fe5786c49a7488d53f00087ab6677f57a0c55a11565f99f'
+  url64bit       = 'https://github.com/rmcrackan/Libation/releases/download/v13.5.1/Libation.13.5.1-windows-chardonnay-x64-setup.exe'
+  checksum64     = '7982d664de1b6db1a0c3874b08b25c5e1a890a96c5776f00315b548edec39cc9'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
--- automatic/libation-classic/tools/chocolateyinstall.ps1
+++ automatic/libation-classic/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'Libation (Classic)'
-  url64bit       = 'https://github.com/rmcrackan/Libation/releases/download/v13.5.0/Libation-Classic.13.5.0-windows-classic-x64-setup.exe'
-  checksum64     = '026e638e97200557451b7bd06e0ad7bd2a0605068383f3569af966a81befce0d'
+  url64bit       = 'https://github.com/rmcrackan/Libation/releases/download/v13.5.1/Libation-Classic.13.5.1-windows-classic-x64-setup.exe'
+  checksum64     = '1fca45bc8db31e74658222b6a8afd4837f9b4b55c094680390523408055d81c4'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup

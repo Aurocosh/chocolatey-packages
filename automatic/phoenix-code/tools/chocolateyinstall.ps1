05b35af38f53e2f5c20f99516923c348ab9f0f48
Aurocosh <aurocosh@gmail.com>
2026-08-10 12:06:12 +0000

    AU: 1 updated - phoenix-code [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/788c246e272f4c93234d5f04b06ab9931c86380a

--- automatic/phoenix-code/tools/chocolateyinstall.ps1
+++ automatic/phoenix-code/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'Phoenix Code*'
-  url64bit       = 'https://github.com/phcode-dev/phoenix-desktop/releases/download/prod-app-v5.1.24/Phoenix.Code_5.1.24_x64-setup.exe'
-  checksum64     = '960908076ef0adcfbbf31a68290363f3ce16891cae067e9be3e73cfee1c44d62'
+  url64bit       = 'https://github.com/phcode-dev/phoenix-desktop/releases/download/prod-app-v5.2.4/Phoenix.Code_5.2.4_x64-setup.exe'
+  checksum64     = 'a3cab1dfb7af7606f0907824e615b30f7ec7366e05c97c7139870be1ae49f516'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/S'  # NSIS

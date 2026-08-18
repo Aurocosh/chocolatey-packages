2a5c2accbfe01471234ae818a80970588269bc92
Aurocosh <aurocosh@gmail.com>
2026-08-18 12:05:03 +0000

    AU: 1 updated - mullvad-browser [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/3100cc3c2434d8a0b913f9c2bbff3c82b947ce94

--- automatic/mullvad-browser/tools/chocolateyinstall.ps1
+++ automatic/mullvad-browser/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'Mullvad Browser*'
-  url64bit       = 'https://github.com/mullvad/mullvad-browser/releases/download/15.0.19/mullvad-browser-windows-x86_64-15.0.19.exe'
-  checksum64     = '1bcc69725207baaf414dbcdb7e844383bbb1b3a7e826194e563a154428db4bc7'
+  url64bit       = 'https://github.com/mullvad/mullvad-browser/releases/download/15.0.20/mullvad-browser-windows-x86_64-15.0.20.exe'
+  checksum64     = '7eb168be0eb88a5517e8a80e162d6d90205c65472604e7c634036e473b83d193'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/S'  # NSIS

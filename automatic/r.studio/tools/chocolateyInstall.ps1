78076029b24f87222a6cecd9d27b383f2610bdd0
Aurocosh <Aurocosh@gmail.com>
2026-08-18 08:40:08 +0700

    r.studio: Fixed installation

--- automatic/r.studio/tools/chocolateyInstall.ps1
+++ automatic/r.studio/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   checksum64     = '9f8461a1ec285d89fbaed7f601bc7ea95505f06d58b868395771a18bf33b11b1'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
-  silentArgs     = '/S'  # NSIS
+  silentArgs     = '/S /allusers'  # NSIS
 }
 
 Install-ChocolateyPackage @packageArgs

69a95e2aa043221f7c70ae64c77452243ec10da0
Aurocosh <aurocosh@gmail.com>
2026-08-19 12:05:25 +0000

    AU: 1 updated - notegen [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/725e1a5c90ab0b7c283e10fb8fd9083c9cb5eb50

--- automatic/notegen/tools/chocolateyinstall.ps1
+++ automatic/notegen/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'NoteGen*'
-  url64bit       = 'https://github.com/codexu/note-gen/releases/download/note-gen-v0.34.1/NoteGen_0.34.1_x64-setup.exe'
-  checksum64     = '74816532e12e07808f0f10e74bd9bfadecf32db345dbdf540825d192f089382f'
+  url64bit       = 'https://github.com/codexu/note-gen/releases/download/note-gen-v0.35.1/NoteGen_0.35.1_x64-setup.exe'
+  checksum64     = '2f12dc9dce080f499aad9e5f2f1ca576030f48079215e9b64ac9ee9c66b83c92'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/S'  # NSIS

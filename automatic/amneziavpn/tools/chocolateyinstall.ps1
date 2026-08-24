e03ca4e6d9a4af05fa201d46b2dafdd2a86fbc88
Aurocosh <aurocosh@gmail.com>
2026-08-24 12:05:24 +0000

    AU: 2 updated - amneziavpn handy [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/78e794ab809fb337309a1d09e0720fba476e28fd

--- automatic/amneziavpn/tools/chocolateyinstall.ps1
+++ automatic/amneziavpn/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'AmneziaVPN*'
-  url64bit       = 'https://github.com/amnezia-vpn/amnezia-client/releases/download/5.0.0.5/AmneziaVPN_5.0.0.5_windows_x64.exe'
-  checksum64     = '7c27606a1b899e325b5a421557adda41bd16c903496e4d42c2e480ce4cc0ec40'
+  url64bit       = 'https://github.com/amnezia-vpn/amnezia-client/releases/download/5.0.1.5/AmneziaVPN_5.0.1.5_windows_x64.exe'
+  checksum64     = '2e898bbd1d639f5066416961a2a458dba7c3455c0e8f49c7f130e9281d700377'
   checksumType64 = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = 'install --confirm-command --accept-messages --auto-answer installationErrorWithCancel=Ignore'

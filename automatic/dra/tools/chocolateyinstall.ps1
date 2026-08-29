6f3cf92652cc695490f01598a2c4f199eb574a3a
Aurocosh <aurocosh@gmail.com>
2026-08-29 00:06:29 +0000

    AU: 4 updated - dopamine dopamine3 dra vibe [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/32917d479fef8f44c035316fd136212390657d87

--- automatic/dra/tools/chocolateyinstall.ps1
+++ automatic/dra/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $packagePath
   softwareName   = 'DRA*'
-  url64bit       = 'https://github.com/devmatteini/dra/releases/download/0.10.2/dra-0.10.2-x86_64-pc-windows-msvc.zip'
-  checksum64     = 'ab4178cfa9ee7a46983336438c2bd100b529d21c6cd6e378be52e814b59b40d2'
+  url64bit       = 'https://github.com/devmatteini/dra/releases/download/0.10.3/dra-0.10.3-x86_64-pc-windows-msvc.zip'
+  checksum64     = '396c44d18a92fe5584e42417f218a312cf6df856a9d966c162466c85de4b1359'
   checksumType64 = 'sha256'
 }
 

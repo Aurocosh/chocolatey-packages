2de6593d7a7dfc659426021a9e0ae7ff168349c4
Aurocosh <aurocosh@gmail.com>
2026-09-16 00:06:11 +0000

    AU: 1 updated - unity-hub [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/d7dac642355c795674de105d318078c5220e3b16

--- automatic/unity-hub/tools/chocolateyinstall.ps1
+++ automatic/unity-hub/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = "Stop"
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = "https://public-cdn.cloud.unity3d.com/hub/prod/3.21.2/UnityHubSetup-3.21.2-x64.exe"
-$checksum64 = "157651a69f6cad252b38630714f0a24e8267ab89959164b000980d0a31bcabe1"
+$url64 = "https://public-cdn.cloud.unity3d.com/hub/prod/3.21.3/UnityHubSetup-3.21.3-x64.exe"
+$checksum64 = "ee8e5ae4153201b87b3813c96b00f1f0397d1558597a941a9ea9fe538f52e5a3"
 
 $installArgs = '/S'  # NSIS
 

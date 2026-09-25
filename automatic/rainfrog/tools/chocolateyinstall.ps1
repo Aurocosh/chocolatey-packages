d50e8842449d458cee403379d42abc45ba7e896c
Aurocosh <aurocosh@gmail.com>
2026-09-25 00:06:03 +0000

    AU: 1 updated - rainfrog [skip ci] https://gist.github.com/Aurocosh/3da8aff3c73f7fb1daf19001c7a4492b/0748effc5808a7b8b4fb63798ffad25149ede046

--- automatic/rainfrog/tools/chocolateyinstall.ps1
+++ automatic/rainfrog/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileFullPath   = $archiveFile
   softwareName   = 'rainfrog*'
-  url64bit       = 'https://github.com/achristmascarl/rainfrog/releases/download/v0.4.5/rainfrog-v0.4.5-x86_64-pc-windows-msvc.tar.gz'
-  checksum64     = '1fe12d9ab5d79eadf3a5c6851cf30e4d75b08f26b2e8e5c8d518de22d7abdb07'
+  url64bit       = 'https://github.com/achristmascarl/rainfrog/releases/download/v0.4.6/rainfrog-v0.4.6-x86_64-pc-windows-msvc.tar.gz'
+  checksum64     = 'b39ad80cb17bc0fa5fa356090a6d49a7b2b9770a266883984da2e34e05549029'
   checksumType64 = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
